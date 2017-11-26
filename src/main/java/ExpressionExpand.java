e31c70df872f365c12849a47eba03b3d79ab7c4a
zhouz88 <zhouz88@uw.edu>
2017-11-26 14:36:48 -0800

    Update ExpressionExpand.java

--- src/main/java/ExpressionExpand.java
+++ src/main/java/ExpressionExpand.java
@@ -1,6 +1,16 @@
 import java.util.*;
+/*
+575. Expression Expand 
+Given an expression s includes numbers, letters and brackets. Number represents the number 
+of repetitions inside the brackets(can be a string or another expression)．Please expand expression to be a string.
 
-public class Solution {
+Example
+s = abc3[a] return abcaaa
+s = 3[abc] return abcabcabc
+s = 4[ac]dy, return acacacacdy
+s = 3[2[ad]3[pf]]xyz, return adadpfpfpfadadpfpfpfadadpfpfpfxyz
+*/
+public class Solution{
     /*
      * @param s: an expression includes numbers, letters and brackets
      * @return: a string
