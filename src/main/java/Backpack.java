b3cbff1fc0bdf82cce43d4ebd95b9dee5b703453
Kerwin <zhouz88@uw.edu>
2017-11-28 16:39:22 -0800

    Create Backpack.java

---
+++ src/main/java/Backpack.java
@@ -0,0 +1,46 @@
+public class Solution {
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @return: The maximum size
+     
+     92. Backpack 
+
+ Description
+ Notes
+ Testcase
+ Judge
+Given n items with size Ai, an integer m denotes the size of a backpack. How full you can fill this backpack?
+
+ Notice
+
+You can not divide any item into small pieces.
+
+Have you met this question in a real interview? Yes
+Example
+If we have 4 items with size [2, 3, 5, 7], the backpack size is 11, we can select [2, 3, 5], so that the max size we can fill this backpack is 10. If the backpack size is 12. we can select [2, 3, 7] so that we can fulfill the backpack.
+
+You function should return the max size we can fill in the given backpack.
+     */
+    public int backPack(int m, int[] A) {
+        // write your code here
+        int n = A.length;
+        boolean[][] dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= m; j++) {
+                if (j >= A[i - 1]) {
+                    dp[i][j] =  dp[i - 1][j] || dp[i - 1][j - A[i - 1]];
+                }else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        for (int j = m; j >= 0; j--) {
+            if (dp[n][j]) {
+                return j;
+            }
+        }
+        return 0;
+    }
+}
