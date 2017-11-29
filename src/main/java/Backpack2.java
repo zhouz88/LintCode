263525dd5a408cb5ae387598ba5dc6e410a2e888
Kerwin <zhouz88@uw.edu>
2017-11-28 16:47:06 -0800

    Create Backpack2.java

---
+++ src/main/java/Backpack2.java
@@ -0,0 +1,43 @@
+public class Solution {
+    /*
+     * @param nums: an integer array and all positive numbers
+     * @param target: An integer
+     * @return: An integer
+     563. Backpack V 
+
+ Description
+ Notes
+ Testcase
+ Judge
+Given n items with size nums[i] which an integer array and all positive numbers. An integer target denotes the size of a backpack. Find the number of possible fill the backpack.
+
+Each item may only be used once
+
+Have you met this question in a real interview? Yes
+Example
+Given candidate items [1,2,3,3,7] and target 7,
+
+A solution set is: 
+[7]
+[1, 3, 3]
+
+     */
+    public int backPackV(int[] nums, int target) {
+        // write your code here
+        int n = nums.length;
+        int[][] dp = new int[n + 1][target + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= target; j++) {
+        
+        if (j >= nums[i - 1]) {
+            dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+        } else {
+            dp[i][j] = dp[i - 1][j];
+        }
+        
+            }
+        }
+        return dp[n][target];
+    }
+}
