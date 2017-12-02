d88b1371c84a888519c483a8c7cee33dae8b34d0
Kerwin <zhouz88@uw.edu>
2017-12-02 15:05:33 -0800

    Update Backpack2.java

--- src/main/java/Backpack2.java
+++ src/main/java/Backpack2.java
@@ -41,3 +41,24 @@ A solution set is:
         return dp[n][target];
     }
 }
+
+//o(n)
+
+public class Solution {
+    /*
+     * @param nums: an integer array and all positive numbers
+     * @param target: An integer
+     * @return: An integer
+     */
+    public int backPackV(int[] nums, int target) {
+        // write your code here
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for(int i = 0; i < nums.length; i++) {
+            for (int j = target; j >= 0; j--) {
+               dp[j] = dp[j] + (j >= nums[i] ? dp[j- nums[i]] :0);
+            }
+        }
+        return dp[target];
+    }
+}
