c3d44e4ebe11867cab324353e72adbe4cb2801ac
Kerwin <zhouz88@uw.edu>
2017-12-02 15:16:52 -0800

    Update Backpack.java

--- src/main/java/Backpack.java
+++ src/main/java/Backpack.java
@@ -75,3 +75,30 @@ public class Solution {
         return -1;
     }
 }
+
+//o(n)
+public class Solution {
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A: Given n items with size A[i]
+     * @return: The maximum size
+     */
+    public int backPack(int m, int[] A) {
+        // write your code here
+        boolean[] dp = new boolean[m + 1];
+        dp[0] = true;
+        for (int i = 0; i <= A.length - 1; i++) {
+            for (int j = m; j >= 0; j--) {
+                if (j - A[i] >= 0) {
+                    dp[j] |= dp[j - A[i]];
+                }
+            }
+        }
+        for (int k = m; k>=0;k--) {
+            if (dp[k]) {
+                return k;
+            }
+        }
+        return -1;
+    }
+}
