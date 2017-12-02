78bea1ea29ef797de146eff9a33d6ed8226c649b
Kerwin <zhouz88@uw.edu>
2017-12-02 15:49:00 -0800

    Create EditDistance1.java

---
+++ src/main/java/EditDistance1.java
@@ -0,0 +1,43 @@
+public class Solution {
+    /*
+     * @param word1: A string
+     * @param word2: A string
+     * @return: The minimum number of steps.
+     119. Edit Distance 
+
+ Description
+ Notes
+ Testcase
+ Judge
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+Insert a character
+Delete a character
+Replace a character
+     */
+    public int minDistance(String word1, String word2) {
+        // write your code here
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    int k = 1 + Math.min(dp[i - 1][j], dp[i][j - 1]);
+                    dp[i][j] = Math.min(k, dp[i - 1][j - 1] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
