ec3fa980d8dde880f4500b551c8daa3db9d18666
Kerwin <zhouz88@uw.edu>
2017-12-03 16:24:57 -0800

    Create Frogjump.java

---
+++ src/main/java/Frogjump.java
@@ -0,0 +1,68 @@
+public class Solution {
+    /*
+     * @param stones: a list of stones' positions in sorted ascending order
+     * @return: true if the frog is able to cross the river or false
+     
+     622. Frog Jump 
+
+ Description
+ Notes
+ Testcase
+ Judge
+A frog is crossing a river. The river is divided into x units and at each unit there may or may not exist a stone. The frog can jump on a stone, but it must not jump into the water.
+
+Given a list of stones' positions (in units) in sorted ascending order, determine if the frog is able to cross the river by landing on the last stone. Initially, the frog is on the first stone and assume the first jump must be 1 unit.
+
+If the frog's last jump was k units, then its next jump must be either k - 1, k, or k + 1 units. Note that the frog can only jump in the forward direction.
+
+ Notice
+
+The number of stones is ≥ 2 and is < 1100.
+Each stone's position will be a non-negative integer < 2^31.
+The first stone's position is always 0.
+Have you met this question in a real interview? Yes
+Example
+Given stones = [0,1,3,5,6,8,12,17]
+
+There are a total of 8 stones.
+The first stone at the 0th unit, second stone at the 1st unit,
+third stone at the 3rd unit, and so on...
+The last stone at the 17th unit.
+
+Return true. The frog can jump to the last stone by jumping 
+1 unit to the 2nd stone, then 2 units to the 3rd stone, then 
+2 units to the 4th stone, then 3 units to the 6th stone, 
+4 units to the 7th stone, and 5 units to the 8th stone.
+
+Given stones = `[0,1,2,3,4,8,9,11]`
+
+Return false. There is no way to jump to the last stone as 
+the gap between the 5th and 6th stone is too large.
+Tags 
+
+     */
+    public boolean canCross(int[] stones) {
+        // write your code here
+        int n = stones.length;
+        Map<Integer, Boolean>[] dp = new HashMap[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = new HashMap<>();
+        }
+        dp[0].put(0, true) ; 
+        for (int i = 1; i <= n - 1; i++) {
+            for (int k = 0; k < i; k++) {
+                int j = stones[i] - stones[k];
+    boolean tmp = (contains(dp[k], j - 1))||contains(dp[k], j)||(contains(dp[k], j + 1));
+                dp[i].put(j, tmp);
+            }
+        }
+        for (int k : dp[n - 1].keySet()) {
+            if (dp[n - 1].get(k)) return true;
+        }
+        return false;
+    }
+    
+    private boolean contains(Map<Integer, Boolean> map, int k) {
+        return map.containsKey(k) && map.get(k);
+    }
+}
