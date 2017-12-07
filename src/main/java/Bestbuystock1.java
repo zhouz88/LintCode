f268cdc6cb07e26c76e117d214607ec07a07b6cb
Kerwin <zhouz88@uw.edu>
2017-12-07 12:33:21 -0800

    Create Bestbuystock1.java

---
+++ src/main/java/Bestbuystock1.java
@@ -0,0 +1,30 @@
+public class Solution {
+    /*
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     149. Best Time to Buy and Sell Stock 
+
+ Description
+ Notes
+ Testcase
+ Judge
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+
+     */
+    public int maxProfit(int[] prices) {
+        // write your code here
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int res = 0;
+        int min = prices[0];
+        for (int i = 1;i < prices.length; i++) {
+            min = Math.min(min, prices[i]);
+            res = Math.max(prices[i] - min, res);
+        }
+        return res;
+    }
+}
