e3ec361181cd7a6d662992cb763a317839f15bdd
Kerwin <zhouz88@uw.edu>
2017-12-07 12:35:41 -0800

    Create Bestbuy2.java

---
+++ src/main/java/Bestbuy2.java
@@ -0,0 +1,25 @@
+public class Solution {
+    /*
+     * @param prices: Given an integer array
+     * @return: Maximum profit
+     150. Best Time to Buy and Sell Stock II 
+
+ Description
+ Notes
+ Testcase
+ Judge
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+     */
+    public int maxProfit(int[] prices) {
+        // write your code here
+        int sum = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i] < prices[i + 1]) {
+                sum += prices[i + 1] - prices[i];
+            }
+        }
+        return sum;
+    }
+}
