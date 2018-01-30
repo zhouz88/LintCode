fc498d09744ea796bb629262c6f1f5b4e423e488
Kerwin <zhouz88@uw.edu>
2018-01-29 18:17:49 -0800

    Update 3sumclosest.java

--- src/main/java/3sumclosest.java
+++ src/main/java/3sumclosest.java
@@ -56,3 +56,30 @@ public class Solution {
         return ans;
     }
 }
+
+
+//o(n2)
+public int threeSumClosest(int[] numbers, int target) {
+        if (numbers == null || numbers.length < 3) {
+            return -1;
+        }
+        
+        Arrays.sort(numbers);
+        int bestSum = numbers[0] + numbers[1] + numbers[2];
+        for (int i = 0; i < numbers.length; i++) {
+            int start = i + 1, end = numbers.length - 1;
+            while (start < end) {
+                int sum = numbers[i] + numbers[start] + numbers[end];
+                if (Math.abs(target - sum) < Math.abs(target - bestSum)) {
+                    bestSum = sum;
+                }
+                if (sum < target) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+        
+        return bestSum;
+    }
