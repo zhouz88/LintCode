426b55bef53ff1a0eeb0fd3afc14dd6236b3cc5c
Kerwin <zhouz88@uw.edu>
2018-01-30 00:45:32 -0800

    Create 111. Climbing Stairs.java

---
+++ src/main/java/111. Climbing Stairs.java
@@ -0,0 +1,19 @@
+public class Solution {
+    /**
+     * @param n: An integer
+     * @return: An integer
+     */
+   public int climbStairs(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        int last = 1, lastlast = 1;
+        int now = 0;
+        for (int i = 2; i <= n; i++) {
+            now = last + lastlast;
+            lastlast = last;
+            last = now;
+        }
+        return now;
+    }
+}
