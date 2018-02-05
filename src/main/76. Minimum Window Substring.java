8ba4ac6fe4a0488db5645b0be82abf744cd840ce
Kerwin <zhouz88@uw.edu>
2018-02-05 14:03:08 -0800

    Create 76. Minimum Window Substring.java

---
+++ src/main/76. Minimum Window Substring.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || t == null) {
+            return "";
+        }
+        int j = 0;
+        int[] window = new int[256];
+        int[] tMap = new int[256];
+        for (char ch : t.toCharArray()) {
+            tMap[ch]++;
+        }
+        String ret = "";
+        int cnt = 0, max = 999999999;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (window[ch] < tMap[ch]) {
+                cnt++;
+            } 
+            window[ch]++;
+            while (cnt == t.length() && j <= i) {
+                if (i - j + 1 < max) {
+                    ret = s.substring(j, i + 1);
+                    max = i - j + 1;
+                }
+                char tmp = s.charAt(j);
+                if (window[tmp] == tMap[tmp]) {
+                    cnt--;
+                }
+                window[tmp]--;
+                j++;
+            }
+        }
+        
+        return ret;
+    }
+}
