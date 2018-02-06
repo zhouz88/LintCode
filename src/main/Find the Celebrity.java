63255664cd5a708a07a2f2ba33f0033fcf5f1031
Kerwin <zhouz88@uw.edu>
2018-02-05 20:54:24 -0800

    Create Find the Celebrity.java

---
+++ src/main/Find the Celebrity.java
@@ -0,0 +1,30 @@
+Suppose you are at a party with n people (labeled from 0 to n - 1) and among them, there may exist one celebrity. The definition of a celebrity is that all the other n - 1 people know him/her but he/she does not know any of them.
+
+Now you want to find out who the celebrity is or verify that there is not one. The only thing you are allowed to do is to ask questions like: "Hi, A. Do you know B?" to get information of whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+
+You are given a helper function bool knows(a, b) which tells you whether A knows B. Implement a function int findCelebrity(n), your function should minimize the number of calls to knows.
+public class Solution extends Relation {
+    /**
+     * @param n a party with n people
+     * @return the celebrity's label or -1
+     */
+    public int findCelebrity(int n) {
+        // Write your code here
+        int ans = 0;
+        for (int i = 1; i < n; i++) {
+            if (knows(ans, i)) {
+                ans = i;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (ans != i && knows(ans, i)) {
+                return -1;
+            }
+            if (ans != i && !knows(i, ans)) {
+                return -1;
+            }
+        }
+        return ans;
+    }
+}
