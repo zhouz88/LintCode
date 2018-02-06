ab85c02f9172e29117ec183451dbc9c2aa66d778
Kerwin <zhouz88@uw.edu>
2018-02-05 20:50:37 -0800

    Create 278. First Bad Version.java

---
+++ src/main/278. First Bad Version.java
@@ -0,0 +1,14 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int l = 1, r = n;
+        while (l <= r) {
+            int mid = ((r - l) >> 1) + l;
+            if (isBadVersion(mid)) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
