7f14dd0b1a8eed810fd7e4b9f2e5f1098139889c
Kerwin <zhouz88@uw.edu>
2018-02-05 17:29:03 -0800

    Create 128. Longest Consecutive Sequence.java

---
+++ src/main/128. Longest Consecutive Sequence.java
@@ -0,0 +1,32 @@
+128. Longest Consecutive Sequence
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                continue;
+            }
+            Integer low = map.get(nums[i] - 1);
+            Integer high = map.get(nums[i] + 1);
+            int total = 1;
+            total += (low == null ? 0 : low);
+            total += (high == null ? 0 : high);
+            map.put(nums[i], total);
+            if (low != null) {
+                map.put(nums[i] - low, total);
+            }
+            if (high != null) {
+                map.put(nums[i] + high, total);
+            }
+            max = Math.max(max, map.get(nums[i]));
+        }
+        return max;
+    }
+}
