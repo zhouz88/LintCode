59b79fac3da7d05ab63c0a92a6cb3c845ab38c0a
Kerwin <zhouz88@uw.edu>
2018-02-07 02:58:41 -0800

    Update 260. Single Number III.java

--- src/main/260. Single Number III.java
+++ src/main/260. Single Number III.java
@@ -18,3 +18,25 @@ class Solution {
         return ret;
     }
 }
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int num = 0;
+        for (int k : nums) {
+            num ^= k;
+        }
+        
+        int target = -num & num;
+        
+        int[] ret = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            if ((nums[i] & target) == target) {
+                ret[0] ^= nums[i];
+            } else {
+                ret[1] ^= nums[i];
+            }
+        }
+        return ret;
+    }
+}
+
