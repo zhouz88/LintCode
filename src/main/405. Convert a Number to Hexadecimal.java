3d5e15371899836da4a087ca51edafde9309c04d
Kerwin <zhouz88@uw.edu>
2018-02-06 20:34:28 -0800

    Create 405. Convert a Number to Hexadecimal.java

---
+++ src/main/405. Convert a Number to Hexadecimal.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String toHex(int num) {
+        if (num == 0) return "0";
+        char[] map = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+        StringBuilder sb = new StringBuilder();
+        while (num != 0) {
+            sb.insert(0, map[num & 0b1111]);
+            num = (num >>> 4);
+        }
+        System.out.println( -16 >> 1); //区别 移动正数部分4， 最后把正数再变为负数
+        System.out.println( -16 >>> 1);//直接移动负数-4的（也就是4的反码+1,
+        return sb.toString();
+    }
+    
+// -1 的 补码（正数的反码 11111111111111111111111111111110+1）为 32个1, 每次 & 1111后得到一个f 
+//   >>> 4 无符号移动四位后 （移动补码）得到 00001111
+    
+    
+    //-4 为 0100
+}
