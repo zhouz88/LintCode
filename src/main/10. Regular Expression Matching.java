e025adad899451c53b2d1e0a1494bf69294a15d6
Kerwin <zhouz88@uw.edu>
2018-02-06 16:10:02 -0800

    Create 10. Regular Expression Matching.java

---
+++ src/main/10. Regular Expression Matching.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        //initial condition
+        int i, j;
+        for (j = 1; j + 1 <= n; j+=2) {
+            if (p.charAt(j - 1) != '*' && p.charAt(j) == '*') {
+                dp[0][j + 1]= true;
+            } else {
+                break;
+            }
+        }
+        //get
+        for (i = 1; i <= m ; i++) {
+            for (j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    if (j > 1 && (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1))) {
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+                    } else if (j > 1) {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                } else if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
