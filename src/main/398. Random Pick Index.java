8c432ea6daaf090e54b2f443ce2f64fe5e5fea9c
Kerwin <zhouz88@uw.edu>
2018-02-07 22:30:32 -0800

    Update 398. Random Pick Index.java

--- src/main/398. Random Pick Index.java
+++ src/main/398. Random Pick Index.java
@@ -68,3 +68,37 @@ class Solution {
  * Solution obj = new Solution(nums);
  * int param_1 = obj.pick(target);
  */
+
+
+import java.util.Random;
+
+class Solution {
+    private int[] nums;
+    private Random random;
+    
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.random = new Random();
+    }
+
+    public int pick(int target) {
+        int cnt = 0;
+        int ans = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (target == nums[i]) {
+                cnt++;
+                if (random.nextInt(cnt) == 0) {
+                    ans = i;
+                }
+            }
+        }
+        
+        return ans;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
