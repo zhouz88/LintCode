327951182b386533ec1d2649678524901ca17236
Kerwin <zhouz88@uw.edu>
2018-02-08 13:48:38 -0800

    Update 202. Happy Number.java

--- src/main/202. Happy Number.java
+++ src/main/202. Happy Number.java
@@ -3,12 +3,15 @@ import java.util.Set;
 
 class Solution {
     public boolean isHappy(int n) {
+        if (n <=0) {
+           return false;
+        }
         Set<Integer> set = new HashSet<>();
         while (true) {
             int total = 0;
             while (n != 0) {
                 int m = n%10;
-                n  = n/10;
+                n = n/10;
                 total += m*m;
             }
             if (total == 1) {
@@ -18,6 +21,7 @@ class Solution {
                 return false;
             }
             set.add(total);
+            n = total;//wrong 1
         }
     }
 }
