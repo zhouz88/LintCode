1c8be299023d3c74b50288bac7d4e5930a2422a6
Kerwin <zhouz88@uw.edu>
2018-02-07 20:47:28 -0800

    Create 400. Nth Digit.java

---
+++ src/main/400. Nth Digit.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int findNthDigit(int n) {
+        long total = 9;
+        long len = 1;
+        while ( n - len*total > 0 ) {
+            n -= len * total;
+            len++;
+            total *= 10;
+        }
+        long start = (long) Math.pow(10, len - 1);
+        long end = (n - 1)/len + start;
+        String res = ""+end;
+        return res.charAt((int)((n - 1)%len)) - '0';//wrong 1
+    }
+}
