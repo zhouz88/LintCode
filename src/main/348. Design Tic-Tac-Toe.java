0de924c9e43cce0b8015b47dd8a1fdc7ece6c763
Kerwin <zhouz88@uw.edu>
2018-02-09 02:13:27 -0800

    Update 348. Design Tic-Tac-Toe.java

--- src/main/348. Design Tic-Tac-Toe.java
+++ src/main/348. Design Tic-Tac-Toe.java
@@ -1,4 +1,17 @@
 class TicTacToe {
+    /*
+    348. Design Tic-Tac-Toe
+DescriptionHintsSubmissionsDiscussSolution
+DiscussPick One
+Design a Tic-tac-toe game that is played between two players on a n x n grid.
+
+You may assume the following rules:
+
+A move is guaranteed to be valid and is placed on an empty block.
+Once a winning condition is reached, no more moves is allowed.
+A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
+Example:
+    */
     private char[][] matrix;
     private int[] horizontal1;
     private int[] vertial1;
