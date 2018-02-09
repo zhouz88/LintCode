514e85ca5650725130201e4677afbed6843ed4f3
Kerwin <zhouz88@uw.edu>
2018-02-09 03:03:59 -0800

    Create 7. Reverse Integer.java

---
+++ src/main/7. Reverse Integer.java
@@ -0,0 +1,12 @@
+7. Reverse Integerclass 
+Solution {
+    public int reverse(int x) {        
+        long num = 0;
+        while (x != 0) {
+            num = num * 10 + x % 10;
+            x /= 10;
+            if (num > Integer.MAX_VALUE || num < Integer.MIN_VALUE) return 0;
+        }
+        return (int) num;
+    }
+}
