7299726555fcb3b430383dcf490c64d679464057
Kerwin <zhouz88@uw.edu>
2018-02-09 04:42:48 -0800

    Update 274. H-Index.java

--- src/main/274. H-Index.java
+++ src/main/274. H-Index.java
@@ -1,4 +1,20 @@
 274. H-Index
+
+//274. H-Index
+DescriptionHintsSubmissionsDiscussSolution
+DiscussPick One
+Given an array of citations (each citation is a non-negative integer) of a researcher, write a function to compute the researcher's h-index.
+
+According to the definition of h-index on Wikipedia: "A scientist has index h if h of his/her N papers have at least h citations each, and the other N − h papers have no more than h citations each."
+
+For example, given citations = [3, 0, 6, 1, 5], which means the researcher has 5 papers in total and each of them had received 3, 0, 6, 1, 5 citations respectively. Since the researcher has 3 papers with at least 3 citations each and the remaining two with no more than 3 citations each, his h-index is 3.
+
+Note: If there are several possible values for h, the maximum one is taken as the h-index.
+
+Credits:
+Special thanks to @jianchao.li.fighter for adding this problem and creating all test cases.
+    
+    ///
 class Solution {
     public int hIndex(int[] citations) {
         int l = 0, r = citations.length;
@@ -21,5 +37,34 @@ class Solution {
             }
         }
         return cnt;
+    }
+}
+
+
+class Solution {
+    public int hIndex(int[] citations) {
+        int l = 0, r = citations.length;
+        while (l <= r) {
+            int mid = ((r - l) >> 1) + l;
+            if (check(mid, citations) >= mid) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return r;
+    }
+
+    private int check(int target, int[] citations) {
+        int l = 0, r = citations.length - 1;
+        while (l <= r) {
+            int mid = ((r - l) >> 1) + l;
+            if (citations[mid] >= target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return citations.length - l;
     }
 }
