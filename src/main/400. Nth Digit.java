b2c4ac16ba0a9d96424e40350d4886637adc2a3e
Kerwin <zhouz88@uw.edu>
2018-02-13 14:30:55 -0800

    Update 400. Nth Digit.java

--- src/main/400. Nth Digit.java
+++ src/main/400. Nth Digit.java
@@ -14,22 +14,20 @@ class Solution {
     }
 }
 
-class Solution{
-public int findNthDigit(int n) {
-
-		int len = 1;
-		long count = 9;
-		int start = 1;
-
-		while (n > len * count) {
-			n -= len * count;
-			len += 1;
-			count *= 10;
-			start *= 10;
-		}
-
-		start += (n - 1) / len;
-		String s = Integer.toString(start);
-		return Character.getNumericValue(s.charAt((n - 1) % len));
-	}
+class Solution {
+    public int findNthDigit(int n) {
+        long total = 9;
+        int count = 1;
+        long len = 1;
+        while ( n - len*total > 0 ) {
+            n -= len * total;
+            len++;
+            total*= 10;
+            count*=10;
+        }
+        long end = (n - 1)/len + count;
+        String res = ""+end;
+        return res.charAt((int)((n - 1)%len)) - '0';//wrong 1
+    }
 }
+
