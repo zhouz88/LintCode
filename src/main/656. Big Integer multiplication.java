0e00340b57c642b7a96f80a6b9dc5e704446f630
Kerwin <zhouz88@uw.edu>
2018-02-13 02:24:05 -0800

    Update 656. Big Integer multiplication.java

--- src/main/656. Big Integer multiplication.java
+++ src/main/656. Big Integer multiplication.java
@@ -1,47 +1,29 @@
-
-public class Solution {
-    /*
-     * @param num1: a non-negative integers
-     * @param num2: a non-negative integers
-     * @return: return product of num1 and num2
-     */
+class Solution {
     public String multiply(String num1, String num2) {
-        // write your code here
-        int[] number1 = new int[num1.length()];
-        int[] number2 = new int[num2.length()];
-        int i, j;
-        
+        int m = num1.length();
+        int n = num2.length();
+        int[] ret = new int[m + n + 1];
+        int i ,  j;
         for (i = 0; i < num1.length(); i++) {
-            number1[i] = num1.charAt(i) - '0';
-        }
-
-        for (i = 0; i < num2.length(); i++) {
-            number2[i] = num2.charAt(i) - '0';
-        }
-
-        int[] ret = new int[221];
-        int len = ret.length, l1 = number1.length, l2 = number2.length;
-        
-        for (i = 0; i < number1.length; i++) {
-            for (j = 0; j < number2.length; j++) {
-                ret[len - 1 - (l1 - i - 1) - (l2 - j - 1)] += number1[i] * number2[j];
+            for (j = 0; j < num2.length(); j++) {
+                ret[m - i - 1 + n - j - 1] += (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
             }
         }
-        
-        for (i = len - 1; i >= 1; i--) {
-            ret[i - 1] += ret[i]/10;
-            ret[i] = ret[i]%10;
+        for (i = 0; i < ret.length - 1; i++) {
+            ret[i + 1] += ret[i]/10;
+            ret[i] %= 10;
         }
-        
         StringBuilder sb = new StringBuilder();
-        for (i = 0; i < ret.length; i++) {
-            if (ret[i] != 0) {
-                break;
-            }
+        int k = ret.length - 1;
+        while (k >= 0 && ret[k] == 0) {
+            k--;
         }
-        for (; i < ret.length; i++) {
+        if (k == -1) {
+            return "0";
+        }
+        for (i = k; i >= 0; i--) {
             sb.append(ret[i]);
         }
-        return sb.toString().equals("") ? "0" : sb.toString();
+        return  sb.toString();
     }
 }
