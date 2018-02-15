c34a590b1846ab65073c3ec922753f0e1f1297c6
Kerwin <zhouz88@uw.edu>
2018-02-15 00:38:26 -0800

    Update 115. Distinct Subsequences.java

--- src/main/115. Distinct Subsequences.java
+++ src/main/115. Distinct Subsequences.java
@@ -28,3 +28,21 @@ class Solution {
         return dp[m - 1][n - 1];
     }
 }
+
+
+class Solution {
+    public int numDistinct(String S, String T) {
+        int m = S.length();
+        int n = T.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i<= m;i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = ((S.charAt(i - 1) == T.charAt(j - 1) ?dp[i - 1][j - 1] : 0)) + dp[i - 1][j];
+            }
+        }
+        return dp[m][n];
+    }
+}
