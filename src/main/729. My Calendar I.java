e4b3407e7e9584d4281aeb48ac40ac2936687060
Kerwin <zhouz88@uw.edu>
2018-02-16 14:56:12 -0800

    Create 729. My Calendar I.java

---
+++ src/main/729. My Calendar I.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class MyCalendar {
+    List<Line> list;
+
+    public MyCalendar() {
+        this.list = new ArrayList<>();
+    }
+
+    public boolean book(int start, int end) {
+         int l = 0, r = list.size() - 1;
+         Line target1 = new Line(start, - 1);
+         Line target2 = new Line(end, 1);
+         
+         while (l <= r) {
+             int mid = ((r - l) >> 1) + l;
+             if (list.get(mid).compareTo(target1) == 0) {
+                 return false;
+             } else if (list.get(mid).compareTo(target1) > 0) {
+                 r = mid - 1;
+             } else {
+                 l = mid  + 1;
+             }
+         }
+         
+         if (r >= 0 && list.get(r).isStart == -1) {
+             return false;
+         }
+         
+         if (l != list.size() && list.get(l).compareTo(target2) <= 0) {
+             return false;
+         } 
+         
+         list.add(l, target2);
+         list.add(l, target1);
+         
+         return true;
+    }
+
+    class Line implements Comparable<Line>{
+        int val;
+        int isStart;
+        public Line(int val, int isStart) {
+            this.val = val;
+            this.isStart = isStart;
+        }
+
+        @Override
+        public int compareTo(Line o) {
+            return Integer.compare(this.val, o.val) == 0 ? Integer.compare(o.isStart, this.isStart)
+                    : Integer.compare(this.val, o.val);
+        }
+    }
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
