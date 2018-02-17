7ca04f302379b54bca948499f150b0d4d404b8dd
Kerwin <zhouz88@uw.edu>
2018-02-17 15:46:39 -0800

    Update 307. Range Sum Query - Mutable.java

--- src/main/307. Range Sum Query - Mutable.java
+++ src/main/307. Range Sum Query - Mutable.java
@@ -1,49 +1,41 @@
 class NumArray {
-    
-    private int[] Bit;
     private int[] nums;
+    private int[] Bits;
 
     public NumArray(int[] nums) {
         this.nums = nums;
-        this.Bit = new int[nums.length + 1];
+        this.Bits = new int[nums.length + 1];
         for (int i = 0; i < nums.length; i++) {
-            init(i, nums[i]);
+            change(i, nums[i]);
         }
     }
     
-    public void init(int i, int val) {
-	i++;
-	while (i < Bit.length) {
-	    Bit[i] += val;
-	    i += (i & -i);
-	}
-   }
+    private void change(int i, int val) {
+        i++;
+        while (i < Bits.length) {
+            Bits[i] += val;
+            i += (i & -i);
+        }
+    }
+    
 
     public void update(int i, int val) {
-        updateTree(i, val - nums[i]);
+        change(i, val - nums[i]);
         nums[i] = val;
     }
 
-    private void updateTree(int i, int val) {
-        i++;
-        while (i < Bit.length) {
-            Bit[i] += val;
-            i += (-i & i);
-        }
-    }
-
     public int sumRange(int i, int j) {
-         return getSum(j) - getSum(i - 1);
+        return sum(j) - sum(i - 1);
     }
     
-    private int getSum(int i) {
+    public int sum(int i) {
         i++;
-        int sum = 0;
+        int total = 0;
         while (i > 0) {
-            sum += Bit[i];
-            i -= (-i & i);
+            total += Bits[i];
+            i -= (-i&i);
         }
-        return sum;
+        return total;
     }
 }
 
