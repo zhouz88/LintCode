aad0a84e688808f06561e6aa1f76df5cb7f13a4b
Kerwin <zhouz88@uw.edu>
2018-02-17 17:09:59 -0800

    Update 307. Range Sum Query - Mutable.java

--- src/main/307. Range Sum Query - Mutable.java
+++ src/main/307. Range Sum Query - Mutable.java
@@ -53,52 +53,47 @@ class NumArray {
 
 public class NumMatrix {
 
-    int[][] tree;
-    int[][] nums;
-    int m;
-    int n;
-    
+    private int[][] Bits;
+    private int[][] matrix;
+    private int m;
+    private int n;
     public NumMatrix(int[][] matrix) {
-        if (matrix.length == 0 || matrix[0].length == 0) 
+         if (matrix.length == 0 || matrix[0].length == 0) 
             return;
-        
-        m = matrix.length;
-        n = matrix[0].length;
-        tree = new int[m + 1][n + 1];
-        nums = new int[m][n];
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                update(i, j, matrix[i][j]);
+         this.matrix = matrix;
+         this.Bits = new int[matrix.length + 1][matrix[0].length + 1];
+         m = matrix.length;
+         n = matrix[0].length;
+         for (int i = 0; i < m;  i++)
+             for(int j = 0; j < n; j++)
+                change(Bits, i, j, matrix[i][j]);
+
+    }
+
+    private void change(int[][] Bits, int row, int col, int val) {
+        for (int i = row + 1; i <= m; i += (i&-i)) {
+            for (int j = col + 1; j <= n; j += (j&-j)) {
+                Bits[i][j] += val;
             }
         }
     }
 
     public void update(int row, int col, int val) {
-        if (m == 0 || n == 0) 
-            return;
-        int delta = val - nums[row][col];
-        nums[row][col] = val;
-        for (int i = row + 1; i <= m; i += i & (-i)) {
-            for (int j = col + 1; j <= n; j += j & (-j)) {
-                tree[i][j] += delta;
-            }
-        }
+        change(Bits, row, col, val - matrix[row][col]);
+        matrix[row][col] = val;
     }
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        if (m == 0 || n == 0) 
-            return 0;
-        return sum(row2 + 1, col2 + 1) + sum(row1, col1) - sum(row1, col2 + 1) - sum(row2 + 1, col1);
+        return sum(row2 , col2) + sum(row1 - 1, col1 - 1) - sum(row2, col1 - 1) - sum(row1 - 1, col2);
     }
-    
-    public int sum(int row, int col) {
-        int sum = 0;
-        for (int i = row; i > 0; i -= i & (-i)) {
-            for (int j = col; j > 0; j -= j & (-j)) {
-                sum += tree[i][j];
+
+    private int sum(int row , int col) {
+        int total = 0;
+        for (int i = row + 1; i > 0; i -= (i&-i)) {
+            for (int j = col + 1; j > 0; j -= (j&-j)) {
+                total += Bits[i][j];
             }
         }
-        return sum;
+        return total;
     }
 }
