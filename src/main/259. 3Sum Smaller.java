847723e76e79196df9be42fbfbe16f9623bc9e37
Kerwin <zhouz88@uw.edu>
2018-02-19 19:41:43 -0800

    Create 259. 3Sum Smaller.java

---
+++ src/main/259. 3Sum Smaller.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        Arrays.sort(nums);
+        int cnt = 0;
+        for (int i = 0; i  < nums.length - 1; i++) {
+            int l = i + 1;
+            int r = nums.length - 1;
+            while (l < r) {
+                int total = nums[i] + nums[l] + nums[r];
+                if (total < target) {
+                    cnt += r - l;
+                    l++;
+                } else if (total >= target) {
+                    r--;
+                } 
+            }
+        }
+        return cnt;
+   
+  }
+}
+
+/*
+259. 3Sum Smaller
+DescriptionHintsSubmissionsDiscussSolution
+Pick One
+Given an array of n integers nums and a target, find the number of index triplets i, j, k with 0 <= i < j < k < n that satisfy the condition nums[i] + nums[j] + nums[k] < target.
+
+For example, given nums = [-2, 0, 1, 3], and target = 2.
+
+Return 2. Because there are two triplets which sums are less than 2:
+
+[-2, 0, 1]
+[-2, 0, 3]
+Follow up:
+Could you solve it in O(n2) runtime?
+*/
