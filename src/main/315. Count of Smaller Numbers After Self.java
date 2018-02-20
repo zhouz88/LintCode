54897633199a4ab3dbc73d358c72e922f3c3c4bf
Kerwin <zhouz88@uw.edu>
2018-02-20 15:37:01 -0800

    Update 315. Count of Smaller Numbers After Self.java

--- src/main/315. Count of Smaller Numbers After Self.java
+++ src/main/315. Count of Smaller Numbers After Self.java
@@ -1,74 +1,67 @@
+import java.util.Arrays;
+import java.util.List;
+
 class Solution {
-	private static int[] count;
-	
-    public static List<Integer> countSmaller(int[] nums) {
-    	List<Integer> result = new ArrayList<Integer>();
-    	
-    	count = new int[nums.length];
-    	int[] index = new int[nums.length];
-    	
-    	for (int i = 0; i < nums.length; i++) {
-    		index[i] = i;
-    	}
-    	
-        mergesort(nums, index, 0, nums.length - 1);
+    private Integer[] counts;
+
+    public List<Integer> countSmaller(int[] nums) {
+        Node[] nodes = new Node[nums.length];
+        this.counts = new Integer[nums.length];
+        Arrays.fill(counts, 0);//important ! bug 1
         
-        for (int i = 0; i < count.length; i++){
-        	result.add(count[i]);
+        for (int i = 0; i < nums.length; i++) {
+            nodes[i] = new Node(nums[i], i);
         }
-    	return result;
-    	
+        
+        mergeSort(nodes, 0, nums.length  - 1);
+        return Arrays.asList(counts);
     }
-    
-    private static void mergesort(int[] nums, int[] index, int start, int end) {
-    	
-    	if (start < end) {
-    		int mid = (start + end) / 2;
-    	
-    		mergesort(nums, index, start, mid);
-    		mergesort(nums, index, mid+1, end);
-    		
-    		merge(nums, index, start, mid, end);
-    	}
+
+    private void mergeSort(Node[] nodes, int start, int end) {
+        if (start < end) {
+            int mid = ((end - start) >> 1) + start;
+            
+            mergeSort(nodes, start, mid);
+            mergeSort(nodes, mid + 1, end);
+            
+            Node[] array = new Node[end - start + 1]; //bug
+            
+            int l = start;
+            int r = mid + 1;
+            int idx = 0;//for array
+            int total = 0;
+
+            while (l <= mid && r <= end) {
+                if (nodes[l].val <= nodes[r].val) {
+                    counts[nodes[l].id] += total;
+                    array[idx++] = nodes[l++];
+                } else {
+                    total++;
+                    array[idx++] = nodes[r++];
+                }
+            }
+            
+            while (l <= mid) {
+                counts[nodes[l].id] += total; //bug 2
+                array[idx++] = nodes[l++];
+            }
+            
+            while (r <= end) {
+                array[idx++] = nodes[r++];
+            }
+
+            for (int i = start; i <= end; i++) {
+                nodes[i] = array[i - start];
+            }
+        }
     }
-    
-    private static void merge(int[] nums, int[] index, int start, int mid, int end) {
-    	int[] tmp = new int[end - start + 1];
-		
-		int i = start; 
-		int j = mid + 1;
-		int k = 0;
-		int rightCount = 0;
-		
 
-		while (i <= mid && j <= end) {
-			if (nums[index[j]] < nums[index[i]]) {
-				tmp[k] = index[j];
-				rightCount++;
-				j++;
-			} else {
-				tmp[k] = index[i];
-				count[index[i]] += rightCount;
-				i++;
-			}
-			k++;
-		}
-
-		while (i <= mid) {
-			tmp[k] = index[i];
-			count[index[i]] += rightCount;
-			k++;
-			i++;
-		}
-		
-		while (j <= end) {
-			tmp[k] = index[j];
-			k++;
-			j++;
-		}
-		
-		for (int m = start; m <= end; m++) {
-			index[m] = tmp[m - start];
-		}
+    private static class Node {
+        int val;
+        int id;
+        public Node(int val, int id) {
+            this.val = val;
+            this.id = id;
+        }
     }
 }
