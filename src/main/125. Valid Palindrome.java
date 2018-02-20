e34dea94defa97ad5e4ba61e664a670b7cdd2389
Kerwin <zhouz88@uw.edu>
2018-02-19 16:36:10 -0800

    Update 125. Valid Palindrome.java

--- src/main/125. Valid Palindrome.java
+++ src/main/125. Valid Palindrome.java
@@ -23,3 +23,33 @@ class Solution {
         return (ch>='a'&& ch<='z')|| (ch>='0'&&ch<='9');
     }
 }
+
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        char[] t = s.toCharArray();
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            
+            while (i < j && !isLorD(t[i])) {
+                i++;
+            }
+            while (i < j && !isLorD(t[j])) {
+                j--;
+            }
+            if (i < j) {
+                if (Character.toLowerCase(t[i]) != Character.toLowerCase(t[j]) ) {
+                    return false;
+                } 
+                i++;
+                j--;
+            }
+        }
+        
+        return true;
+    }
+
+    public boolean isLorD(char ch) {
+        return (ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z');
+    }
+}
