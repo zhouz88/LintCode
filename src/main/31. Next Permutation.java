ec6e66a4458a5dbf6c638c21491af9614e49cf12
Kerwin <zhouz88@uw.edu>
2018-02-19 16:28:02 -0800

    Update 31. Next Permutation.java

--- src/main/31. Next Permutation.java
+++ src/main/31. Next Permutation.java
@@ -32,3 +32,42 @@ class Solution {
         nums[j] = tmp;
     }
 }
+
+
+//2
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int i = nums.length - 1, j = i;
+        
+        int len = nums.length;
+        
+        while (i > 0 && nums[i - 1] >= nums[i]) {
+            i--;
+        }
+        
+        if (i == 0) {
+            reverse(0, len - 1, nums);
+            return;
+        }
+        
+        while (j > 0 && nums[j] <= nums[i - 1]) {
+            j--;
+        }
+        
+        int tmp = nums[i - 1];
+        nums[i - 1] = nums[j];
+        nums[j] = tmp;
+        
+        reverse(i, len - 1, nums);
+    }
+
+    private void reverse(int i, int j, int[] nums) {
+        while (i < j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            i++;
+            j--;
+        }
+    }
+}
