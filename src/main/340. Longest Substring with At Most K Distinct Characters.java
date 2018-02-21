86c84990c678d28abc1ab29cac954730e2126223
Kerwin <zhouz88@uw.edu>
2018-02-21 01:15:29 -0800

    Update 340. Longest Substring with At Most K Distinct Characters.java

--- src/main/340. Longest Substring with At Most K Distinct Characters.java
+++ src/main/340. Longest Substring with At Most K Distinct Characters.java
@@ -1,3 +1,15 @@
+/*
+340. Longest Substring with At Most K Distinct Characters
+DescriptionHintsSubmissionsDiscussSolution
+Pick One
+Given a string, find the length of the longest substring T that contains at most k distinct characters.
+
+For example, Given s = “eceba” and k = 2,
+
+T is "ece" which its length is 3.
+
+
+*/
 class Solution {
     public int lengthOfLongestSubstringKDistinct(String s, int k) {
         //edge case
