840f61d28324a7d58b49a3cd8fb38fc8e79ba9e8
Kerwin <zhouz88@uw.edu>
2018-02-21 03:02:51 -0800

    Update 621. Task Scheduler.java

--- src/main/621. Task Scheduler.java
+++ src/main/621. Task Scheduler.java
@@ -62,3 +62,28 @@ class Solution {
         }
     }
 }
+
+
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        if (tasks == null) {
+            return 0;
+        }
+        
+        int[] count = new int[26];
+        for (char task : tasks) {
+            count[task - 'A']++;
+        }
+        
+        Arrays.sort(count);
+        
+        int i = 25;
+        int max = count[i];
+        
+        while (i >= 0 && count[i] == max) {
+            i--;
+        }
+        
+        return Math.max(tasks.length, (max - 1) * (n + 1) + 25 - i);
+    }
+}
