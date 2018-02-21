b07fcbd6f6d0a414423dcf75cd97c5dabd8518a6
Kerwin <zhouz88@uw.edu>
2018-02-20 16:46:45 -0800

    Update 493. Reverse Pairs.java

--- src/main/493. Reverse Pairs.java
+++ src/main/493. Reverse Pairs.java
@@ -1,3 +1,5 @@
+//binary search
+
 class Solution {
     private int count = 0;
 
@@ -64,6 +66,73 @@ class Solution {
             }
         }
         return l;
+    }
+
+    private static class Node {
+        long val;
+        int id;
+        public Node(long val, int id){
+            this.val = val;
+            this.id = id;
+        }
+    }
+}
+
+//two pointers 
+
+class Solution {
+    private int count = 0;
+
+    public int reversePairs(int[] nums) {
+        Node[] nodes = new Node[nums.length];
+
+        for (int i = 0; i < nums.length; i++)
+            nodes[i] = new Node(nums[i], i);
+
+        mergeSort(nodes, 0, nums.length - 1);
+
+        return count;
+    }
+
+    private void mergeSort(Node[] nodes, int start, int end) {
+        if (start < end) {
+            int mid = ((end - start) >> 1) + start;
+            mergeSort(nodes, start, mid);
+            mergeSort(nodes, mid + 1, end);
+
+            Node[] array = new Node[end - start + 1];
+            int idx = 0;
+            int l = start, r = mid + 1;
+            int j = mid + 1;
+            
+            for (int i = start; i <= mid; i++) {
+                long g = 2 * nodes[j].val;
+                if (nodes[i].val > g) {
+                    count += mid - i + 1;
+                    j++;
+                }
+            }
+
+            while (l <= mid && r <= end) {
+                if (nodes[l].val <= nodes[r].val) {
+                    array[idx++] = nodes[l++];
+                } else {
+                    array[idx++] = nodes[r++];
+                }
+            }
+
+            while (l <= mid) {
+                array[idx++] = nodes[l++];
+            }
+
+            while (r <= end) {
+                array[idx++] = nodes[r++];
+            }
+
+            for (int i = start; i <= end; i++) {
+                nodes[i] = array[i - start];
+            }
+        }
     }
 
     private static class Node {
