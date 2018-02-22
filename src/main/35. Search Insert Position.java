a49bf96930f91e766ef1309ffd79257985a516d2
Kerwin <zhouz88@uw.edu>
2018-02-21 18:31:29 -0800

    Create 35. Search Insert Position.java

---
+++ src/main/35. Search Insert Position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = ((r - l) >> 1) + l;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return l;
+    }
+}
