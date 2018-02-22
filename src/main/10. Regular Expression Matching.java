c1b2fce049e56f2b698a439405778e35a467881c
Kerwin <zhouz88@uw.edu>
2018-02-21 16:33:00 -0800

    Update 10. Regular Expression Matching.java

--- src/main/10. Regular Expression Matching.java
+++ src/main/10. Regular Expression Matching.java
@@ -31,3 +31,36 @@ class Solution {
         return dp[m][n];
     }
 }
+
+
+//good
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        //initial condition
+        int i, j;
+        for (j = 1; j <= n; j ++) {
+            dp[0][j] = (j >= 2 && dp[0][j - 2] && p.charAt(j - 1)=='*');
+        }
+        
+        //get
+        for (i = 1; i <= m ; i++) {
+            for (j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '*') {
+                    if (j > 1 && (p.charAt(j - 2) == '.' || p.charAt(j - 2) == s.charAt(i - 1))) {
+                        dp[i][j] = dp[i][j - 2] || dp[i - 1][j];
+                    } else if (j > 1) {
+                        dp[i][j] = dp[i][j - 2];
+                    }
+                } else if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
