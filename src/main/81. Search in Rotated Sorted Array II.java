df83de9cf127e0b094310307ed8d0f3e01884fef
Kerwin <zhouz88@uw.edu>
2018-02-21 21:16:01 -0800

    Update 81. Search in Rotated Sorted Array II.java

--- src/main/81. Search in Rotated Sorted Array II.java
+++ src/main/81. Search in Rotated Sorted Array II.java
@@ -48,3 +48,40 @@ class Solution {
         return false;
     }
 }
+
+//no du
+class Solution {
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        
+        int l = 0, r = nums.length - 1;
+        
+        while (l <= r) {
+            int mid = ((r - l) >> 1) + l;
+            
+            if (nums[mid] == target) {
+                return mid;
+            }
+            
+            if (nums[mid] >= nums[0]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            }
+
+            if (nums[mid] <= nums[nums.length - 1]) {
+                if (nums[mid] < target && target <= nums[nums.length - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        
+        return -1;
+    }
+}
