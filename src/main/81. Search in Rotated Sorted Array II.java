c4debbd0a88a6963c277dabd6ec79caf98e4fd77
Kerwin <zhouz88@uw.edu>
2018-02-21 21:14:56 -0800

    Create 81. Search in Rotated Sorted Array II.java

---
+++ src/main/81. Search in Rotated Sorted Array II.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+
+        int l = 0, r = nums.length - 1;
+        
+        if (nums[l] < nums[r]) {//no rotation
+            int idx = Arrays.binarySearch(nums, 0 , nums.length, target);
+            if (idx < 0) {
+                return false;
+            }
+            return true;
+        }
+        
+        while (l <= r) {
+            int mid = ((r - l) >> 1) + l;
+            
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[mid] > nums[0]) { //how to make sure is left?
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } 
+            
+            if (nums[mid] < nums[nums.length - 1]) {
+                if (nums[mid] < target && target <= nums[nums.length - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+            
+            if (nums[mid] == nums[l]) {
+                l++;
+            } else if (nums[mid] == nums[r]){
+                r--;
+            }
+            
+        }
+
+        return false;
+    }
+}
