09a13d30a548f34ab8ceadf1860e353d1d896a04
Kerwin <zhouz88@uw.edu>
2018-02-23 01:24:38 -0800

    Update 190. Reverse Bits.java

--- src/main/190. Reverse Bits.java
+++ src/main/190. Reverse Bits.java
@@ -2,7 +2,7 @@ public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
         int a = 0;
-        for (int i = 0; i <= 32; i++) {
+        for (int i = 0; i <= 31; i++) {
             int k = n >> i;
             if ((k & 1) == 1) a |= (1 << (31 - i));
         }
