68a864f25e242fd07ec6801f16c5bc18fc4dbb9c
Kerwin <zhouz88@uw.edu>
2018-02-24 21:25:29 -0800

    Update 271. Encode and Decode Strings.java

--- src/main/271. Encode and Decode Strings.java
+++ src/main/271. Encode and Decode Strings.java
@@ -1,26 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Codec {
 
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
         StringBuilder sb = new StringBuilder();
-        for(String s : strs) {
-            sb.append(s.length()).append('/').append(s);
+        for (String K : strs) {
+            K = K.replace("/", "//");
+            sb.append(" / ");
+            sb.append(K);
+            sb.append(" / ");
         }
         return sb.toString();
     }
 
-    // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
-        List<String> ret = new ArrayList<String>();
-        int i = 0;
-        while(i < s.length()) {
-            int slash = s.indexOf('/', i);
-            int size = Integer.valueOf(s.substring(i, slash));
-            ret.add(s.substring(slash + 1, slash + size + 1));
-            i = slash + size + 1;
+        List<String> res = new ArrayList<>();
+        String[] t = s.split(" / ");
+        for (int i = 0; i < s.length(); i++) {
+            if (s.startsWith(" / ", i)) {
+                int idx = s.indexOf(" / ", i + 1);
+                String g = (s.substring(i + 3, idx)).replace("//", "/");
+                res.add(g);
+                i = idx + 2;
+            }
         }
-        return ret;
+        return res;
     }
+
 }
 
 // Your Codec object will be instantiated and called as such:
