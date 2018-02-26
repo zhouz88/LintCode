12049fd6973f58cd4365d19891369089351ed70e
Kerwin <zhouz88@uw.edu>
2018-02-26 14:37:24 -0800

    Create 338. Counting Bits.java

---
+++ src/main/338. Counting Bits.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] countBits(int num) {
+        if (num == 0) {
+            return new int[]{0};
+        }
+        int[] dp = new int[num + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= num; i++) {
+            dp[i] = dp[i >> 1] + dp[i & 1];
+        }
+        return dp;
+    }
+}
