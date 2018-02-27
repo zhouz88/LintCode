c7e4ec7d443721be758880fbe90f7811fc761a11
Kerwin <zhouz88@uw.edu>
2018-02-26 23:49:53 -0800

    Update 471. Encode String with Shortest Length.java

--- src/main/471. Encode String with Shortest Length.java
+++ src/main/471. Encode String with Shortest Length.java
@@ -34,3 +34,46 @@ Example 5:
 Input: "abbbabbbcabbbabbbc"
 Output: "2[2[abbb]c]"
 Explanation: "abbbabbbc" occurs twice, but "abbbabbbc" can also be encoded to "2[abbb]c", so one answer can be "2[2[abbb]c]".
+public class Solution {
+    public String encode(String s) {
+        int n = s.length();
+        String[][] dp = new String[n][n];
+
+        //initital condition
+        int i, j , k;
+        for (i = 0; i < s.length(); i++) {
+            dp[i][i] = s.charAt(i) + "";
+        }
+        
+        //next
+        for (int len = 1; len <= n - 1; len++) {
+            for (i = 0; i < n; i++) {
+                j = Math.min(i + len, n - 1);//bug 2
+                String cur = dp[i][i];
+                
+                for (k = i; k < j; k++) {
+                    String left = dp[i][k];
+                    String right = dp[k + 1][j];
+                    if (cur.length() == 1) {
+                        cur = left + right;
+                    } else if (cur.length() > left.length() + right.length()) {
+                        cur = left + right;
+                    }
+                }
+                
+                String total = s.substring(i, j +  1);//bug3
+                int idx = (total + total).indexOf(total, 1);
+                if (idx > 0 && idx < total.length()) {//bug 1
+                    int f = total.length()/idx;
+                    String newString = f + "[" + dp[i][i + idx - 1] + "]";
+                    if (newString.length() <= cur.length()) {
+                        cur = newString;
+                    }
+                }
+               
+                dp[i][j] = cur;
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
