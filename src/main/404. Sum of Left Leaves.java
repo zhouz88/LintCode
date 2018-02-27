55001cb167d92809e93239de0b91939a6f0d1719
Kerwin <zhouz88@uw.edu>
2018-02-27 14:35:47 -0800

    Create 404. Sum of Left Leaves.java

---
+++ src/main/404. Sum of Left Leaves.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            return root.left.val + sumOfLeftLeaves(root.right) + sumOfLeftLeaves(root.left);
+        } else {
+            return sumOfLeftLeaves(root.right) + sumOfLeftLeaves(root.left);
+        }
+    }
+  
+}
