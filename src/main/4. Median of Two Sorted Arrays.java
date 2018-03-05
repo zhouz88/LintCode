f1f0bddca8eef95c2342831c2cd2b7547bf563bc
Kerwin <zhouz88@uw.edu>
2018-03-05 14:45:32 -0800

    Update 4. Median of Two Sorted Arrays.java

--- src/main/4. Median of Two Sorted Arrays.java
+++ src/main/4. Median of Two Sorted Arrays.java
@@ -2,38 +2,45 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         int m = nums1.length, n = nums2.length;
         if ((m + n) % 2 == 0) {
-            return 1.0*(findKth(nums1, nums2, (m + n)/2, m, n) + findKth(nums1, nums2, (m + n)/2 + 1, m, n))/2;
+            return 1.0 * (findKth(nums1, nums2, (m + n)/2, m, n) + findKth(nums1, nums2, (m + n)/2 + 1, m, n))/2;
         } else {
             return 1.0 * findKth(nums1, nums2, (m + n + 1)/2, m, n);
         }
     }
 
     private int findKth(int[] nums1, int[] nums2, int k, int m, int n) {
-        int l = 0;
-        int r = 0;
+        int l = 0, r = 0;
+        
         while (true) {
             if (l == m) {
                 return nums2[r + k - 1];
             }
+            
             if (r == n) {
                 return nums1[l + k - 1];
             }
+            
             if (k == 1) {
                 return Math.min(nums1[l], nums2[r]);
             }
             
-            if (l + k/2 - 1 < m && r + k/2 - 1 < n) {
-                if (nums1[l + k/2 - 1] < nums2[r + k/2 - 1]) {
+            int first = l + k/2 - 1;
+            int second = r + k/2 - 1;
+            
+            if (first < m && second < n) {
+                if (nums1[first] < nums2[second]) {
                     l = l + k/2;
                 } else {
                     r = r + k/2;
                 }
-            } else if (l + k/2 - 1 < m) {
+            } else if (first < m) {
                 l = l + k/2;
             } else {
                 r = r + k/2;
             }
+            
             k -= k/2;
         }
+        
     }
 }
