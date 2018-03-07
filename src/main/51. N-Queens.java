a7d703f990b868d4a2b15eb9d43ca9d86870a1ef
Kerwin <zhouz88@uw.edu>
2018-03-07 01:43:44 -0800

    Update 51. N-Queens.java

--- src/main/51. N-Queens.java
+++ src/main/51. N-Queens.java
@@ -1,64 +1,54 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 class Solution {
     public List<List<String>> solveNQueens(int n) {
-        List<int[]> ret = new ArrayList<>();
-        if (n == 0) {
-            return new ArrayList<>();
-        }
-        int[] path = new int[n];
-        Arrays.fill(path, -1);
-        update(path, n, 0, ret);
-        
-        char[][] sb = new char[n][n];
-        for (char[] ch : sb) {
-            Arrays.fill(ch, '.');
-        }
         List<List<String>> res = new ArrayList<>();
-        for (int[] k : ret) {
+        int[] path = new int[n];
+        List<int[]> pathList = new ArrayList<>();
+        dfs(path, 0, pathList);
+        
+        StringBuilder[] sb = new StringBuilder[n];
+        for (int i = 0; i < n; i++) {
+            sb[i] = new StringBuilder();
+            for (int j = 0; j < n; j++) {
+                sb[i].append('.');
+            }
+        }
+        for (int[] paths : pathList) {
             List<String> list = new ArrayList<>();
-            for (int u : k) {
-                int i  = u/n;
-                int j = u%n;
-                sb[i][j] = 'Q';
-                list.add(new String(sb[i]));
-                sb[i][j] = '.';
+            for (int i = 0; i < n; i++) {
+                int y = paths[i];
+                sb[i].setCharAt(y, 'Q');
+                list.add(sb[i].toString());
+                sb[i].setCharAt(y, '.');
             }
             res.add(list);
         }
         return res;
     }
 
-    private void update(int[] path, int n, int start, List<int[]> ret) {
-        if (start == n) {
-            ret.add(Arrays.copyOf(path, n));
+    private void dfs(int[] path, int i, List<int[]> pathList) {
+        if (i == path.length) {
+            pathList.add(Arrays.copyOf(path, path.length));
             return;
         }
-        for (int j = 0; j < n; j++) {
-            boolean flag = false;
-            for (int i = 0; i < start; i++) {
-                if (!ok(path[i], (start)*n+j, n)) {
-                    flag = true;
-                    break;
-                }
-            }
-            if (!flag) {
-                path[start] = start * n + j;
-                update(path, n, start + 1, ret);
+        for (int j = 0; j < path.length; j++) {
+            if (isValid(path, i, j)) {
+                path[i] =  j;
+                dfs(path, i + 1, pathList);
+                path[i] = 0;
             }
         }
     }
-    
-    private boolean ok(int pre, int now, int n) {
-        int i = pre/n;
-        int j = pre%n;
-        int I = now/n;
-        int J = now%n;
-        if (J == j || Math.abs(I - i) == Math.abs(J - j)) {
-            return false;
+
+    private boolean isValid(int[] path, int i, int j) {
+        for (int k = 0; k < i; k++) {
+            int y = path[k];
+            if (y == j || Math.abs(k - i) == Math.abs(y - j)) {
+                return false;
+            }
         }
         return true;
     }
