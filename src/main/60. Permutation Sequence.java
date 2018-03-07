f49ab437eeaa576abf0806bbb32d2f8e19b0f0fc
Kerwin <zhouz88@uw.edu>
2018-03-07 12:41:08 -0800

    Update 60. Permutation Sequence.java

--- src/main/60. Permutation Sequence.java
+++ src/main/60. Permutation Sequence.java
@@ -1,28 +1,32 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.TreeSet;
 
 class Solution {
     public String getPermutation(int n, int k) {
-        int[] dp = new int[10];
+        List<Integer> list = new ArrayList<>();
+        int[] dp = new int[n + 1];
         dp[0] = 1;
-        for (int i = 1; i < 10; i++) {
+
+        for (int i = 1; i <= n; i++) {
             dp[i] = dp[i - 1] * i;
         }
-        List<Integer> list = new ArrayList<>();
-        for (int i = 1;i <= n; i++) {
+
+        for (int i = 1; i <= n; i++) {
             list.add(i);
         }
+        
         StringBuilder sb = new StringBuilder();
-        while(list.size() != 0) {
-            int a = (k - 1)/dp[n - 1];
-            int b = (k - 1)%dp[n - 1];
-            Integer tmp = list.get(a);
-            sb.append(tmp);
-            list.remove(tmp);
+        k--;
+        
+        while (n >= 1) {
+            int row = k/dp[n - 1];
+            int col = k%dp[n - 1];
+            sb.append(list.get(row));
+            list.remove(row);
+            k = col;
             n --;
-            k = b + 1;
         }
+        
         return sb.toString();
     }
 }
