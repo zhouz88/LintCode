d3dc620073dbc3a30335dcfdd90d3845e3f18ba1
Kerwin <zhouz88@uw.edu>
2018-03-08 20:18:01 -0800

    Update 67. Add Binary.java

--- src/main/67. Add Binary.java
+++ src/main/67. Add Binary.java
@@ -29,3 +29,50 @@ class Solution {
         return sb.toString();
     }
 }
+//for
+class Solution {
+    public String addBinary(String a, String b) {
+        int m = a.length();
+        int n = b.length();
+        int[] res = new int[m + n + 1];
+        int i, j;
+        int idx = 0;
+        int carry = 0;
+        
+        for (i = a.length() - 1, j = b.length() - 1; i >= 0 && j >= 0; idx++, i--, j--) {
+            int total = a.charAt(i) - '0' + b.charAt(j) - '0' + carry;
+            res[idx] = total%2;
+            carry = total/2;
+        }
+        
+        for (;i >= 0; i--) {
+            int total = a.charAt(i) - '0' + carry;
+            res[idx++] = total%2;
+            carry = total/2;
+        }
+
+        for (;j >= 0; j--) {
+            int total = b.charAt(j) - '0' + carry;
+            res[idx++] = total%2;
+            carry = total/2;
+        }
+        
+        if (carry == 1) {
+            res[idx++] = 1;
+        }
+        
+        for (i = res.length - 1; i >= 0; i--) {
+            if (res[i] == 1) {
+                break;
+            }
+        }
+        if (i == -1) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (;i >= 0; i--) {
+            sb.append(res[i]);
+        }
+        return sb.toString();
+    }
+}
