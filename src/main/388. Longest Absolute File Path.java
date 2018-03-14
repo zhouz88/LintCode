2f99993f5eecefe412ce8e347f4e58ba4669ce7f
Kerwin <zhouz88@uw.edu>
2018-03-13 22:29:43 -0700

    Update 388. Longest Absolute File Path.java

--- src/main/388. Longest Absolute File Path.java
+++ src/main/388. Longest Absolute File Path.java
@@ -38,3 +38,49 @@ class Solution {
         return i;
     }
 }
+
+//
+import java.util.Stack;
+
+class Solution {
+    public int lengthLongestPath(String input) {
+        String[] t = input.split("\n");
+        Stack<String> stk = new Stack<>();
+        int max = 0;
+        for (int i = 0; i < t.length; i++) {
+            int o = cntOft(t[i]);
+            if (stk.isEmpty()) {
+                stk.add(t[i]);
+                if (t[i].contains(".")) {
+                    max = Math.max(max, len(stk));
+                }
+            } else if (o > cntOft(stk.peek())) {
+                stk.add(t[i]);
+                if (t[i].contains(".")) {
+                    max = Math.max(max, len(stk));
+                }
+            } else if (cntOft(stk.peek()) >= o){
+                while (!stk.isEmpty() && cntOft(stk.peek()) >= o) {
+                    stk.pop();
+                }
+                stk.add(t[i]);
+                if (t[i].contains(".")) {
+                    max = Math.max(max, len(stk));
+                }
+            }
+        }
+        return max;
+    }
+
+    private int len(Stack<String> stk) {
+        int sum = 0;
+        for (int i = 0; i < stk.size(); i++) {
+            sum += stk.get(i).length() - cntOft(stk.get(i));
+        }
+        return sum + stk.size() - 1;
+    }
+
+    private int cntOft(String t) {
+        return t.lastIndexOf("\t") + 1;
+    }
+}
