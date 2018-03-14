70403adef492e8e3ea2a04ca1555f31562762c86
Kerwin <zhouz88@uw.edu>
2018-03-14 12:17:35 -0700

    Update 336. Palindrome Pairs.java

--- src/main/336. Palindrome Pairs.java
+++ src/main/336. Palindrome Pairs.java
@@ -100,4 +100,46 @@ class Solution {
     }
 }
 
+//
+import java.util.*;
 
+class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> res = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < words.length; i++) {
+            map.put(words[i], i);
+        }
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j <= words[i].length(); j++) {
+                String str1 = words[i].substring(0, j);
+                String str2 = words[i].substring(j);
+                if (isPa(str1)) {
+                    String reverseString = new StringBuilder(str2).reverse().toString();
+                    if (map.containsKey(reverseString) && map.get(reverseString) != i) {
+                        res.add(Arrays.asList(map.get(reverseString), i));
+                    }
+                }
+                if (isPa(str2) && str2.length() != 0) {
+                    String reverseString = new StringBuilder(str1).reverse().toString();
+                    if (map.containsKey(reverseString) && map.get(reverseString) != i) {
+                        res.add(Arrays.asList(i, map.get(reverseString)));
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+    private boolean isPa(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l) !=  s.charAt(r)) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
