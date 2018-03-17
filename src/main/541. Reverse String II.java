4222c845fae9f38497a4ce8b565699bdf35251f5
Kerwin <zhouz88@uw.edu>
2018-03-17 15:21:09 -0700

    Update 541. Reverse String II.java

--- src/main/541. Reverse String II.java
+++ src/main/541. Reverse String II.java
@@ -1,35 +1,19 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        if (s == null || s.length() == 0) {
-            return s;
-        }
-        int n = 0;
+        int idx = 0;
         StringBuilder sb = new StringBuilder();
-        StringBuilder ret = new StringBuilder();
-        int pre = 0;
-
         for (int i = 0; i < s.length(); i++) {
-            int row = i/(2*k);
-            int col = (i%(2*k))/k;
-
-            if (row == pre) {
-                if (col == 0) sb.insert(0, s.charAt(i));
-                else sb.append(s.charAt(i));
-            } else {
-                ret.append(sb.toString());
-                pre++;
-                sb.setLength(0);
-                if (col == 0) sb.insert(0, s.charAt(i));
-                else sb.append(s.charAt(i));
+            char ch = s.charAt(i);
+            if (idx < k) {
+                sb.insert(i - idx, ch);
+            } else if (idx < 2 * k) {
+                sb.append(ch);
+            } 
+            idx++;
+            if (idx == 2 * k) {
+                idx = 0;
             }
         }
-
-        ret.append(sb);
-
-        return ret.toString();
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Solution().reverseStr("abcdefghijklmnopqrst", 3));
+        return sb.toString();
     }
 }
