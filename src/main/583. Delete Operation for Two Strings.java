3084c446519597f526f99bd810accabef0ea516b
Kerwin <zhouz88@uw.edu>
2018-03-18 04:04:59 -0700

    Create 583. Delete Operation for Two Strings.java

---
+++ src/main/583. Delete Operation for Two Strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        dp[0][0] = 0;
+        int i, j;
+        for (i = 1; i <= m; i++) {
+            for (j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return word1.length() + word2.length() - 2 * dp[m][n];
+    }
+}
