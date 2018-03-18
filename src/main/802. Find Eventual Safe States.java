6706fa812478e9a0b4bedb11bad9e6c89b4f91cf
Kerwin <zhouz88@uw.edu>
2018-03-17 21:07:49 -0700

    Create 802. Find Eventual Safe States.java

---
+++ src/main/802. Find Eventual Safe States.java
@@ -0,0 +1,55 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        List<Integer> res = new ArrayList<>();
+        int n = graph.length;
+        boolean[] visited = new boolean[n];
+        int[] hasCycle = new int[n];
+        for (int i = 0; i < n; i++) {
+            visited[i] = true;
+            if (dfs(i, visited, graph, hasCycle)) {
+                res.add(i);
+            } else {
+                hasCycle[i] = -1;
+            }
+            visited[i] = false;
+        }
+        Collections.sort(res);
+        return res;
+    }
+
+    private boolean dfs(int start, boolean[] visited, int[][] graph, int[] hasCycle) {
+        if (graph[start].length == 0) {
+            return true;
+        }
+
+        if (hasCycle[start] == -1) {
+            return false;
+        }
+
+        if (hasCycle[start] == 1) {
+            return true;
+        }
+        
+
+        for (int i = 0; i < graph[start].length; i++) {
+            if (visited[graph[start][i]]) {
+                hasCycle[start] = -1;
+                hasCycle[graph[start][i]] = -1;
+                return false;
+            }
+            visited[graph[start][i]] = true;
+            if (!dfs(graph[start][i], visited, graph, hasCycle)) {
+                visited[graph[start][i]] = false;
+                hasCycle[graph[start][i]] = -1;
+                return false;
+            }
+            visited[graph[start][i]] = false;
+        }
+        hasCycle[start] = 1;
+        return true;
+    }
+}
