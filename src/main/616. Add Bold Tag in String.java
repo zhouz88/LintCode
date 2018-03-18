ddfdc1909e67572ebf6c6053a370a968b9be0394
Kerwin <zhouz88@uw.edu>
2018-03-18 12:28:35 -0700

    Update 616. Add Bold Tag in String.java

--- src/main/616. Add Bold Tag in String.java
+++ src/main/616. Add Bold Tag in String.java
@@ -5,9 +5,9 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             for (int j = 0; j < dict.length; j++) {
                 if (s.startsWith(dict[j], i)) {
-                    array[i] = 1;
+                    array[i] += 1;
                     if (i + dict[j].length() < s.length()) {
-                        array[i + dict[j].length()] = -1;
+                        array[i + dict[j].length()] -= 1;
                     }
                 }
             }
@@ -21,7 +21,7 @@ class Solution {
 
         StringBuilder sb = new StringBuilder();
         int cnt = 0;
-
+        
         for (int i = 0; i < s.length(); i++) {
             if (array[i] > 0) {
                 sb.append(s.charAt(i));
