72b0e5f61dad5dbdcadef35efa96b203381f981e
Kerwin <zhouz88@uw.edu>
2018-03-19 15:24:13 -0700

    Rename 729. My Calendar I.java to 729. My Calendar.java

---
+++ src/main/729. My Calendar.java
@@ -0,0 +1,38 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+class MyCalendar{
+
+    private TreeMap<Integer, Integer> map;
+
+    public MyCalendar() {
+        this.map = new TreeMap<>();
+    }
+
+    public boolean book(int start, int end) {
+        map.put(start, map.getOrDefault(start, 0) + 1);
+        map.put(end, map.getOrDefault(end, 0) - 1);
+        int sum = 0;
+        for (Map.Entry<Integer, Integer> e : map.headMap(end, true).entrySet()) {
+            sum += e.getValue();
+            if (sum >= 2) {
+                map.put(start, map.get(start) - 1);
+                map.put(end, map.get(end) + 1);
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo obj = new MyCalendarTwo();
+ * boolean param_1 = obj.book(start,end);
+ */
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree obj = new MyCalendarThree();
+ * int param_1 = obj.book(start,end);
+ */
