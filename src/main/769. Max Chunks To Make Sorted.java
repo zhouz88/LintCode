4a72348eb55dd8344c744462f2b29df057add460
Kerwin <zhouz88@uw.edu>
2018-03-19 20:34:06 -0700

    Create 769. Max Chunks To Make Sorted.java

---
+++ src/main/769. Max Chunks To Make Sorted.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxChunksToSorted(int[] arr) {
+        int cnt = 0;
+        int max = 0;
+        for (int i = 0; i < arr.length; i++) {
+            max = Math.max(arr[i], max);
+            if (max == i) {
+        
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
