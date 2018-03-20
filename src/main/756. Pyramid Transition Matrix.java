d849608a441aa4d0484242d467d1efcb0e70ba56
Kerwin <zhouz88@uw.edu>
2018-03-19 19:19:13 -0700

    Create 756. Pyramid Transition Matrix.java

---
+++ src/main/756. Pyramid Transition Matrix.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public boolean pyramidTransition(String bottom, List<String> allowed) {
+        Map<String, List<Character>> map = new HashMap<>();
+
+        for (int i = 0; i < allowed.size();i++) {
+            map.putIfAbsent(allowed.get(i).substring(0, 2), new ArrayList<>());
+            map.get(allowed.get(i).substring(0, 2)).add(allowed.get(i).charAt(2));
+        }
+
+        return dfs(bottom, map);
+    }
+
+    private boolean dfs(String bottom, Map<String, List<Character>> map) {
+        if (bottom.length() == 1) {
+            return true;
+        }
+        List<String> bottoms = new ArrayList<>();
+        getString(bottoms, bottom, new StringBuilder(), map, 0);
+        for (int i = 0; i < bottoms.size(); i++) {
+            if (dfs(bottoms.get(i), map)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void getString(List<String> bottoms, String bottom, StringBuilder stringBuilder, Map<String, List<Character>> map, int start) {
+        if (start == bottom.length() - 1) {
+            bottoms.add(stringBuilder.toString());
+            return;
+        }
+        String tmp = bottom.substring(start, start + 2);
+        if (map.containsKey(tmp)) {
+            for (char ch : map.get(tmp)) {
+                stringBuilder.append(ch);
+                getString(bottoms, bottom, stringBuilder, map, start + 1);
+                stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+            }
+        }
+    }
+}
