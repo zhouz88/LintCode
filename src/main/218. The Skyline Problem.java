ead850d463c334f073647a327aae2340e17d5790
Kerwin <zhouz88@uw.edu>
2018-03-21 11:30:33 -0700

    Update 218. The Skyline Problem.java

--- src/main/218. The Skyline Problem.java
+++ src/main/218. The Skyline Problem.java
@@ -48,3 +48,60 @@ class Solution {
         }
     }
 }
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.TreeMap;
+
+class Solution {
+    public List<int[]> getSkyline(int[][] buildings) {
+        List<int[]> res = new ArrayList<>();
+        PriorityQueue<Line> pq = new PriorityQueue<>();
+        for (int[] building: buildings) {
+            pq.add(new Line(building[0], -1, building[2]));
+            pq.add(new Line(building[1], 1, building[2]));
+        }
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(0, 1);
+        int preMax = 0;
+        while (!pq.isEmpty()) {
+            Line node = pq.poll();
+            if (node.isStart == -1) {
+                map.put(node.height, map.getOrDefault(node.height, 0) + 1);
+            } else {
+                map.put(node.height, map.get(node.height) - 1);
+                if (map.get(node.height) == 0) {
+                    map.remove(node.height);
+                }
+            }
+            int maxHeight = map.descendingKeySet().iterator().next();
+            if (maxHeight != preMax) {
+                res.add(new int[]{node.val, maxHeight});
+                preMax = maxHeight;
+            }
+        }
+        return res;
+    }
+
+    private static class Line implements Comparable<Line>{
+        int val;
+        int isStart;
+        int height;
+        public Line(int val, int isStart, int height) {
+            this.val = val;
+            this.isStart = isStart;
+            this.height = height;
+        }
+
+        @Override
+        public int compareTo(Line o) {
+            return Integer.compare(this.val, o.val) == 0 ?
+                    Integer.compare(this.isStart * this.height, o.isStart * o.height)
+                    : Integer.compare(this.val, o.val);
+
+        }
+    }
+}
+
