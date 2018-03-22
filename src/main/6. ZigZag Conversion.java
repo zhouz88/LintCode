3d42fa06ab1a2415e6e1e32045c696053dc428c2
Kerwin <zhouz88@uw.edu>
2018-03-22 00:57:07 -0700

    Update 6. ZigZag Conversion.java

--- src/main/6. ZigZag Conversion.java
+++ src/main/6. ZigZag Conversion.java
@@ -1,34 +1,33 @@
 class Solution {
-    public String convert(String s, int m) {
-         //edge case
-        if (m <= 1) {
-            return s;
-        }
-        //general s.length() > 2;
-        int n = 2 * m - 2;
-        
-        int j;
-        StringBuilder[] strs = new StringBuilder[m];
-        
-        for (int i = 0; i < strs.length; i++) {
-            strs[i] = new StringBuilder();
-        }
-        int len = s.length();
-        
-        for (int i = 0; i < len; i += n) {
-            for (j = i; j < Math.min(m + i, len); j++) {
-                strs[j - i].append(s.charAt(j)); //wrong 1 j not i
-            }
-            int index = m - 2;
-            for (j = i + m; j < Math.min(n + i, len); j++) {
-                strs[index--].append(s.charAt(j));
-            }
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        for (StringBuilder k:  strs) {
-            sb.append(k);
-        }
-        return sb.toString();
+    public String convert(String s, int numRows) {
+       if (numRows <= 1) {
+           return s;
+       }
+       int len = 2 * numRows - 2;
+       StringBuilder[] sb = new StringBuilder[numRows];
+       for (int i = 0; i < sb.length; i++) {
+           sb[i] = new StringBuilder();
+       }
+       int idx1 = 0;
+       int idx2 = 0;
+       for (int i = 0; i < s.length(); i++) {
+           if (idx1 < numRows) {
+               idx1++;
+               sb[idx2++].append(s.charAt(i));
+           } else {
+               idx1++;
+               idx2--;
+               sb[idx2 - 1].append(s.charAt(i));
+           }
+           if (idx1 == len) {
+               idx1 = 0;
+               idx2 = 0;
+           }
+       }
+       StringBuilder res = new StringBuilder();
+       for (int i = 0; i < sb.length; i++) {
+           res.append(sb[i]);
+       }
+       return res.toString();
     }
 }
