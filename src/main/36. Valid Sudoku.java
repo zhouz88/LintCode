5debe892f7ac5d08b3d5d979daaa2ba3b154e27a
Kerwin <zhouz88@uw.edu>
2018-03-22 14:23:12 -0700

    Update 36. Valid Sudoku.java

--- src/main/36. Valid Sudoku.java
+++ src/main/36. Valid Sudoku.java
@@ -1,41 +1,31 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[] check;
-        int i, j;
-        for (i = 0;i < 9; i++) {
-            check = new boolean[256];
-            for (j = 0; j < 9; j++) {
+        for (int i = 0; i < 9; i++) {
+            boolean[] col = new boolean[128];
+            boolean[] row = new boolean[128];
+            boolean[] cubic = new boolean[128];
+            int starti = 3 * (i/3);
+            int startj = 3 * (i%3);
+            for (int j =  0; j < 9; j++) {
                 if (board[i][j] != '.') {
-                    if (check[board[i][j]]) return false;
-                    check[board[i][j]] = true;
+                    if (row[board[i][j]]) return false;
+                    else row[board[i][j]] = true;
+                }
+                
+                if (board[j][i] != '.') {
+                    if (col[board[j][i]]) return false;
+                    else col[board[j][i]] = true;
+                }
+                
+                int I = j/3 + starti;
+                int J = j%3 + startj;
+                
+                if (board[I][J] != '.') {
+                    if (cubic[board[I][J]]) return false;
+                    else cubic[board[I][J]] = true;
                 }
             }
         }
-
-        for (j = 0; j < 9; j++) {
-            check = new boolean[256];
-            for (i = 0; i < 9; i++) {
-                if (board[i][j] != '.') {
-                    if (check[board[i][j]]) return false;
-                    check[board[i][j]] = true;
-                }
-            }
-        }
-
-        for (i = 0; i <= 6; i += 3) {
-            for (j = 0; j <= 6; j += 3) {
-                check = new boolean[256];
-                for (int x = i; x < i + 3; x++) {
-                    for (int y = j; y < j + 3; y++) {
-                        if (board[x][y] != '.') {
-                            if (check[board[x][y]]) return false;
-                            check[board[x][y]] = true;
-                        }
-                    }
-                }
-            }
-        }
-        
         return true;
     }
 }
