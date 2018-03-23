ff87636308b08c08f5bf251d7ab1f2fa014a52d6
Kerwin <zhouz88@uw.edu>
2018-03-23 00:43:48 -0700

    Update 164. Maximum Gap.java

--- src/main/164. Maximum Gap.java
+++ src/main/164. Maximum Gap.java
@@ -1,44 +1,52 @@
-import java.util.Arrays;
+import java.math.BigInteger;
 
-public class Solution {
-    public int maximumGap(int[] a) {
-        if (a == null || a.length < 2)
+class Solution {
+    public int maximumGap(int[] nums) {
+        if (nums.length <= 1) {
             return 0;
-        int m = a.length;
-        int max = a[0];
-        int min = a[0];
-        for (int k : a) {
-            max = Math.max(max, k);
-            min = Math.min(min, k);
         }
-        int gap = (int)Math.ceil((1.0* max - 1.0* min)/(m - 1));
+        if (nums.length == 2) {
+            return Math.abs(nums[0] - nums[1]);
+        }
+        int min = nums[0];
+        int max = nums[0];
 
-        int[] bucketsMin = new int[m - 1];
-        int[] bucketsMax = new int[m - 1];
-        Arrays.fill(bucketsMax, Integer.MIN_VALUE);
-        Arrays.fill(bucketsMin, Integer.MAX_VALUE);
+        int len = nums.length;
 
-        for (int i = 0; i < a.length; i++) {
-            if (a[i] == min || a[i] == max) {
-                continue;
+        for (int i = 0; i < nums.length; i++) {
+            min = Math.min(min, nums[i]);
+            max = Math.max(max, nums[i]);
+        }
+
+        int gap = (int) (Math.ceil((double)((long)max - (long)min)/(len*1.0 - 1)));
+        Node[] buckets = new Node[len - 1];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == min||nums[i] ==max) continue;
+            int idx = (nums[i] - min)/gap;
+            if (buckets[idx] == null) {
+                buckets[idx] = new Node(Integer.MAX_VALUE, Integer.MIN_VALUE);
             }
-            int idx = (a[i] - min)/gap;//本质上是用CPU 计算换区排序速度。
-            bucketsMax[idx] = Math.max(bucketsMax[idx], a[i]);
-            bucketsMin[idx] = Math.min(bucketsMin[idx], a[i]);
+            buckets[idx].minValue = Math.min(buckets[idx].minValue, nums[i]);
+            buckets[idx].maxValue = Math.max(buckets[idx].maxValue, nums[i]);
         }
-//bucket 肯定同时有值
-        int maxGap = Integer.MIN_VALUE;
-        int previous = min;
-        for (int i = 0; i < a.length - 1; i++) {
-            if (bucketsMin[i] == Integer.MAX_VALUE && bucketsMax[i] == Integer.MIN_VALUE)
-                // empty bucket
-                continue;
-            // min value minus the previous value is the current gap
-            maxGap = Math.max(maxGap, bucketsMin[i] - previous);
-            // update previous bucket value
-            previous = bucketsMax[i];
+        int pre = min;
+        int res = 0;
+        for (int i = 0; i < buckets.length; i++) {
+            if (buckets[i] == null) continue;
+            res = Math.max(buckets[i].minValue - pre, res);
+            pre = buckets[i].maxValue;
         }
-        maxGap = Math.max(maxGap, max - previous);
-        return maxGap;
+        res = Math.max(max - pre, res);
+        return res;
+    }
+
+    private static class Node {
+        int minValue;
+        int maxValue;
+        public Node(int a, int b) {
+            this.maxValue = b;
+            this.minValue = a;
+        }
     }
 }
