cbfcf15f13fce270ba4f1cc6915ce3ad4d01720f
Kerwin <zhouz88@uw.edu>
2018-03-23 16:50:35 -0700

    Update 296. Best Meeting Point.java

--- src/main/296. Best Meeting Point.java
+++ src/main/296. Best Meeting Point.java
@@ -1,43 +1,37 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 class Solution {
     public int minTotalDistance(int[][] grid) {
-       List<Integer> up = new ArrayList<>();
-       List<Integer> horizontal = new ArrayList<>();
-
-       int i , j;
-       for (i = 0; i < grid.length; i++) {
-           for (j = 0; j < grid[0].length; j++) {
-               if (grid[i][j] == 1) {
-                   up.add(i);
-                   horizontal.add(j);//wrong 1 all the ones be should in the list!
-               }
-           }
-       }
-
-       if (up.size() == 0) {
-           return 0;
-       }
-        
-       Collections.sort(up);
-       Collections.sort(horizontal);
-
-       int midUp = up.get((up.size() - 1)/2);
-       int midHo = horizontal.get((horizontal.size() - 1)/2);
-
-       int total = 0;
-
-        for (i = 0; i < grid.length; i++) {
-            for (j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == 1) {
-                    total += Math.abs(midHo - j) + Math.abs(midUp - i);
+        List<Integer> I  = new ArrayList<>();
+        List<Integer> J  = new ArrayList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] ==1) {
+                    I.add(i);
                 }
             }
         }
-
-        return total;
+        for (int j = 0; j < grid[0].length; j++) {
+            for (int i = 0; i < grid.length; i++) {
+                if (grid[i][j] ==1) {
+                    J.add(j);
+                }
+            }
+        }
+        
+        return get(I) + get(J);
+    }
+    
+    private int get(List<Integer> I) {
+        int sum = 0;
+        int i = 0, j = I.size() - 1;
+        while (i < j) {
+            sum += I.get(j) - I.get(i);
+            i++;
+            j--;
+        }
+        return sum;
     }
 }
 /*
