4a9b951c9abb53ce3ef22e89139398c7782f9252
Kerwin <zhouz88@uw.edu>
2018-03-23 13:52:05 -0700

    Update 498. Diagonal Traverse.java

--- src/main/498. Diagonal Traverse.java
+++ src/main/498. Diagonal Traverse.java
@@ -1,18 +1,23 @@
 class Solution {
     public int[] findDiagonalOrder(int[][] matrix) {
-        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return new int[0];
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return new int[0];
+        }
         int m = matrix.length, n = matrix[0].length;
-        int[] res = new int[m *n];
+        int[] res = new int[m * n];
         int idx = 0;
-        for (int sum = 0; sum <= m + n - 2; sum ++) {
-            if (sum % 2 == 0) {
-                for (int i = Math.min(sum, m - 1); i >= Math.max(0, sum - n + 1); i--) 
-                    res[idx++] = matrix[i][sum - i];
+        for (int len = 0; len <= m + n - 2; len++) {
+            if (len % 2 == 0) {
+                for (int i = Math.min(len, m - 1); i >= len - Math.min(n - 1, len); i--) {
+                    res[idx++] = matrix[i][len - i];
+                }
             } else {
-                for (int j = Math.min(sum, n - 1); j >= Math.max(0, sum - m + 1); j--) 
-                    res[idx++] = matrix[sum - j][j];               
+                for (int j = Math.min(len, n - 1); j >= len - Math.min(m - 1, len); j--) {
+                    res[idx++] = matrix[len - j][j];
+                }
             }
         }
         return res;
     }
 }
+
