361a3babba416c29f56fc78c50d49c5cafe6bf74
Kerwin <zhouz88@uw.edu>
2018-03-23 20:02:28 -0700

    Update 207. Course Schedule.java

--- src/main/207. Course Schedule.java
+++ src/main/207. Course Schedule.java
@@ -40,3 +40,44 @@ class Solution {
         return false;
     }
 }
+
+//
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<Integer>[] adjList = new ArrayList[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < prerequisites.length; i++) {
+            adjList[prerequisites[i][0]].add(prerequisites[i][1]);
+        }
+        int[] state = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            if (hasCycle(state, adjList, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean hasCycle(int[] state, List<Integer>[] adjList, int start) {
+        if (state[start] == 1) {
+            return false;
+        }
+        if (state[start] == -1) {
+            return true;
+        }
+        state[start] = -1;
+        for (int i = 0; i < adjList[start].size(); i++) {
+            if (hasCycle(state, adjList, adjList[start].get(i))) {
+                return true;
+            }
+        }
+        state[start] = 1;
+        return false;
+    }
+}
