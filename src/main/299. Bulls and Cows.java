75af137527ca5b129bd73204fb89e56829a994f6
Kerwin <zhouz88@uw.edu>
2018-03-23 17:10:56 -0700

    Update 299. Bulls and Cows.java

--- src/main/299. Bulls and Cows.java
+++ src/main/299. Bulls and Cows.java
@@ -1,25 +1,20 @@
 class Solution {
     public String getHint(String secret, String guess) {
-        char[] t = secret.toCharArray();
-        int[] map = new int[256];
-        for (char ch : t) {
-            map[ch]++;
-        }
-        int A = 0, B = 0;
-        for (int i = 0; i < guess.length(); i++) {
-            if (guess.charAt(i) == t[i]) {
-                A++;
-                map[t[i]]--;
+        int[] sMap = new int[256];
+        int[] gMap = new int[256];
+        int bulls = 0;
+        int cows = 0;
+        for (int i = 0; i < secret.length(); i++) {
+            if (secret.charAt(i) == guess.charAt(i)) {
+                bulls++;
+            } else {
+                sMap[secret.charAt(i)]++;
+                gMap[guess.charAt(i)]++;
             }
         }
-        for (int i = 0; i < guess.length(); i++) {
-            if (guess.charAt(i) != t[i]) {
-                if (map[guess.charAt(i)] > 0) {
-                    map[guess.charAt(i)]--;
-                    B++;
-                }
-            }
+        for (int i = 0; i < 256; i++) {
+            cows += Math.min(sMap[i], gMap[i]);
         }
-        return A + "A" + B + "B";
+        return bulls + "A" + cows + "B";
     }
 }
