2e6e579be1f5db07c6acd149345e5bb0a430d599
Kerwin <zhouz88@uw.edu>
2018-03-26 16:13:47 -0700

    Update 157. Read N Characters Given Read4.java

--- src/main/157. Read N Characters Given Read4.java
+++ src/main/157. Read N Characters Given Read4.java
@@ -1,17 +1,5 @@
-157. Read N Characters Given Read4157. Read N Characters Given Read4
 /* The read4 API is defined in the parent class Reader4.
-      int read4(char[] buf); 
-      157. Read N Characters Given Read4
-DescriptionHintsSubmissionsDiscussSolution
-
-The API: int read4(char *buf) reads 4 characters at a time from a file.
-
-The return value is the actual number of characters read. For example, it returns 3 if there is only 3 characters left in the file.
-
-By using the read4 API, implement the function int read(char *buf, int n) that reads n characters from the file.
-
-Note:
-The read function will only be called once for each test case. */
+      int read4(char[] buf); */
 
 public class Solution extends Reader4 {
     /**
@@ -20,28 +8,16 @@ public class Solution extends Reader4 {
      * @return    The number of characters read
      */
     public int read(char[] buf, int n) {
-        char[] ch = new char[4];
-        //n can be larget than file and file also can be small than n
-        int i = 0;
-        while (i < n) { //should be i < n not true cause n can be 0
-            int size = read4(ch);
-            if (size == 4) {
-                for (int j = 0; j < 4; j++) {
-                    buf[i++] = ch[j];
-                    if (i == n) {
-                        break;
-                    }
-                }
-            } else {
-                for (int j = 0; j < size; j++) {
-                    buf[i++] = ch[j]; // care i == n;
-                    if (i == n) {
-                        break;
-                    }
-                }
-                break;
+        int idx = 0;
+        char[] temp = new char[4];
+        while (idx < n) {
+            int cnt = read4(temp);
+            if (cnt == 0) break;
+            int pointer = 0;
+            while (pointer < cnt && idx < n) {
+                buf[idx++] = temp[pointer++];
             }
         }
-        return i;
+        return idx;
     }
 }
