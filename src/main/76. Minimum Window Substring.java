08d9b39e4b51ac1f8d43d89ec856d731d16b792f
Kerwin <zhouz88@uw.edu>
2018-03-26 03:33:53 -0700

    Update 76. Minimum Window Substring.java

--- src/main/76. Minimum Window Substring.java
+++ src/main/76. Minimum Window Substring.java
@@ -1,33 +1,26 @@
 class Solution {
     public String minWindow(String s, String t) {
         if (s == null || s.length() == 0) 
-            return "";   
+            return "";
+        int min = Integer.MAX_VALUE, start = 0;
         int[] map = new int[256];
-        int[] window = new int[256];
-        int start = 0, end = 0, min = Integer.MAX_VALUE;
-        for (char ch : t.toCharArray())
+        for (char ch : t.toCharArray()) {
             map[ch]++;
-        
-        for (int i = 0, j = 0, cnt = 0; i < s.length(); i++) {
-            if (map[s.charAt(i)] == 0) continue;
-            if (window[s.charAt(i)]++ < map[s.charAt(i)]) {
-                 cnt++;
+        }
+        for (int i = 0, cnt = t.length(), j = 0; i < s.length(); i++) {
+            if (map[s.charAt(i)]-- > 0) {
+                cnt--;
             }
-            while (cnt == t.length() && j <= i) {
+            while (cnt == 0 && j <= i) {
                 if (i - j + 1 < min) {
-                    start = j;
-                    end = i;
                     min = i - j + 1;
+                    start = j;
                 }
-                if (map[s.charAt(j)] == 0) {
-                    j++;
-                    continue;
+                if (++map[s.charAt(j++)] > 0) {
+                    cnt++;
                 }
-                if (window[s.charAt(j)]-- == map[s.charAt(j)])
-                     cnt--;
-                j++;
             }
         }
-        return min == Integer.MAX_VALUE ? "" : s.substring(start, end + 1);
+        return min == Integer.MAX_VALUE ? "" : s.substring(start, start + min);
     }
 }
