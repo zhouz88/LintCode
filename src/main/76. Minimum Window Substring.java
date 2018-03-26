3f4a4ab518493192ed3e16169a5798f8d80f760a
Kerwin <zhouz88@uw.edu>
2018-03-26 03:01:26 -0700

    Update 76. Minimum Window Substring.java

--- src/main/76. Minimum Window Substring.java
+++ src/main/76. Minimum Window Substring.java
@@ -1,35 +1,36 @@
 class Solution {
     public String minWindow(String s, String t) {
-        if (s == null || t == null) {
-            return "";
-        }
-        int j = 0;
-        int[] window = new int[256];
-        int[] tMap = new int[256];
-        for (char ch : t.toCharArray()) {
-            tMap[ch]++;
-        }
-        String ret = "";
-        int cnt = 0, max = 999999999;
-        
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            if (window[ch]++ < tMap[ch]) {
-                cnt++;
-            } 
-            while (cnt == t.length() && j <= i) {
-                if (i - j + 1 < max) {
-                    ret = s.substring(j, i + 1);
-                    max = i - j + 1;
-                }
-                char tmp = s.charAt(j);
-                if (window[tmp]-- == tMap[tmp]) {
-                    cnt--;
-                }
-                j++;
-            }
-        }
-        
-        return ret;
+       if (s == null || s.length() == 0) {
+           return "";
+       }
+       int len = 1;
+       int[] map = new int[256];
+       int[] window = new int[256];
+       for (char ch : t.toCharArray()) {
+           map[ch]++;
+       }
+       int start = 0, end = 0, min = Integer.MAX_VALUE;
+       for (int i = 0, j = 0, count = 0; i < s.length(); i++) {
+           if (map[s.charAt(i)] == 0) continue;
+           if (window[s.charAt(i)]++ < map[s.charAt(i)]) {
+               count++;
+           }
+           while (count == t.length() && j <= i) {
+               if (i - j + 1 < min) {
+                   start = j;
+                   end = i;
+                   min = i - j + 1;
+               }
+               if (map[s.charAt(j)] == 0) {
+                   j++;
+                   continue;
+               }
+               if (window[s.charAt(j)]-- == map[s.charAt(j)]) {
+                   count--;
+               }
+               j++;
+           }
+       }
+       return min == Integer.MAX_VALUE ? "" : s.substring(start, end + 1);
     }
 }
