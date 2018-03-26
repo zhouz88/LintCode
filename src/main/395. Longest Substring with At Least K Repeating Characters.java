7c1884b75104547e5f4317a9cb39a30b37b19a40
Kerwin <zhouz88@uw.edu>
2018-03-26 02:42:25 -0700

    Update 395. Longest Substring with At Least K Repeating Characters.java

--- src/main/395. Longest Substring with At Least K Repeating Characters.java
+++ src/main/395. Longest Substring with At Least K Repeating Characters.java
@@ -1,36 +1,39 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-        //corner case
-        if (s == null || s.length() == 0 || k == 0) {
-            return 0;
+        int max = 0;
+        for (int i = 1; i <= 26; i++) {
+            max = Math.max(helper(s, k, i), max);
         }
-        //use a cache to store the frequencies of this character
-        int[] map = new int[26];
-        for (char ch : s.toCharArray()) {
-            map[ch - 'a'] ++;
-        }
-        String lessThanK = "";
-        
-        for (int i = 0; i < map.length; i++) {
-            if (map[i] != 0 && map[i] < k) {
-                lessThanK += ((char) (i + 'a'));
+        return max;
+    }
+
+    private int helper(String s, int k, int number) {
+        int j = 0;
+        int count = 0;
+        int max = 0;
+        int[] map = new int[128];
+        int okCount = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (map[s.charAt(i)]++ == 0) {
+                count++;
+            }
+            if (map[s.charAt(i)] == k) {
+                okCount++;
+            }
+            while (count > number && j <= i) {
+                map[s.charAt(j)]--;
+                if (map[s.charAt(j)] == 0) {
+                    count--;
+                }
+                if (map[s.charAt(j)] == k - 1) {
+                    okCount--;
+                }
+                j++;
+            }
+            if (okCount == number) {
+               max = Math.max(max, i - j + 1);
             }
         }
-        
-        if (lessThanK.equals("")) {
-            return s.length();
-        }
-        
-        for (char ch : lessThanK.toCharArray()) {
-            s = s.replace(ch+"", " ");
-        }
-        String[] str = s.split("\\s+");
-        
-        int max = 0;
-        for (String t : str) {
-            max = Math.max(longestSubstring(t, k) , max);
-        }
-        
         return max;
     }
 }
