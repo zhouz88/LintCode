17983afc9326b28dda4da083af218ef4cdf42f04
Kerwin <zhouz88@uw.edu>
2018-03-27 20:43:15 -0700

    Update 91. Decode Ways.java

--- src/main/91. Decode Ways.java
+++ src/main/91. Decode Ways.java
@@ -47,3 +47,58 @@ class Solution {
         return dp[s.length()];
     }
 }
+
+//*
+class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        long[] dp = new long[1 + s.length()];
+        dp[0] = 1L;
+        for (int i = 1; i < dp.length; i++) {
+            if (s.charAt(i - 1) == '*') {
+                dp[i] += 9 * dp[i - 1];
+            } else {
+                if (s.charAt(i - 1) != '0') {
+                    dp[i] += dp[i - 1];
+                }
+            }
+            //two;
+            if (i > 1) {
+                if (s.charAt(i - 1) == '*') {
+                    if (s.charAt(i - 2) == '*') {
+                        dp[i] += 15 * dp[i - 2];
+                    } else if (s.charAt(i - 2) == '0'){
+                        continue;
+                    } else {
+                        int count = 0;
+                        int tmp = s.charAt(i - 2) - '0';
+                        for (int j = 1; j <= 9; j++) {
+                            if (tmp * 10 + j >= 10 && tmp * 10 + j <= 26) {
+                                count++;
+                            }
+                        }
+                        dp[i] += count * dp[i - 2];
+                    }
+                } else {//number
+                    if (s.charAt(i - 2) == '*') {
+                        for (int j = 1; j <= 9; j++) {
+                            int tmp = 10 * j + s.charAt(i - 1) - '0';
+                            if (tmp >= 10 && tmp <= 26) {
+                                dp[i] += dp[i - 2];
+                            }
+                        }
+                    } else {
+                        int tmp = 10 * (s.charAt(i - 2) - '0') + s.charAt(i - 1) - '0';
+                        if (tmp >= 10 && tmp <= 26) {
+                            dp[i] += dp[i - 2];
+                        }
+                    }
+                }
+            }
+            dp[i] %= 1000000007L;
+        }
+        return (int) dp[s.length()];
+    }
+}
