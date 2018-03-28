d1d21e4c6377f2e97b84f885254d2acba35a63e5
Kerwin <zhouz88@uw.edu>
2018-03-27 19:48:53 -0700

    Update 271. Encode and Decode Strings.java

--- src/main/271. Encode and Decode Strings.java
+++ src/main/271. Encode and Decode Strings.java
@@ -34,3 +34,71 @@ public class Codec {
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
 // codec.decode(codec.encode(strs));
+//
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strs.size(); i++) {
+            String cur = strs.get(i);
+            sb.append(" / ");
+            String change = change(cur);
+            sb.append(change);
+            sb.append(" / ");
+        }
+        return sb.toString();
+    }
+
+    private String change(String cur) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < cur.length(); i++) {
+            if (cur.charAt(i) == '/') {
+                sb.append("//");
+            } else {
+                sb.append(cur.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String s) {
+        List<String> res = new ArrayList<>();
+        if ("".equals(s)) {
+            return res;
+        }
+        for (int i = 0; i < s.length();) {
+            if (s.startsWith(" / ", i)) {
+                int end = s.indexOf(" / ", i + 1);
+                String cur = s.substring(i + 3, end);
+                res.add(recover(cur));
+                i = end + 3;
+            }
+        }
+        return res;
+    }
+    
+
+    private String recover(String cur) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < cur.length();) {
+            if (i + 1 < cur.length() && cur.charAt(i) == '/' && cur.charAt(i + 1) == '/') {
+                sb.append('/');
+                i += 2;
+            } else {
+                sb.append(cur.charAt(i));
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+    
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
