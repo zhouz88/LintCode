7b94c9b8ab9fd1dc9d8689f3558d9f9ba8b1ff11
Kerwin <zhouz88@uw.edu>
2018-03-27 20:16:46 -0700

    Update 91. Decode Ways.java

--- src/main/91. Decode Ways.java
+++ src/main/91. Decode Ways.java
@@ -24,3 +24,26 @@ class Solution {
         return dp[1];
     }
 }
+
+//*
+class Solution {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        int[] dp = new int[1 + s.length()];
+        dp[0] = 1;
+        for (int i = 1; i < dp.length; i++) {
+            if (s.charAt(i - 1) != '0') {
+                dp[i] += dp[i - 1];
+            }
+            if (i > 1) {
+                int tmp = 10 * (s.charAt(i - 2) - '0') + s.charAt(i - 1) - '0';
+                if (tmp >= 10 && tmp <= 26) {
+                    dp[i] += dp[i - 2]; 
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+}
