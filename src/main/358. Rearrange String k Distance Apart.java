3dd8aa3738e29d65ac6f32c690e25341cfa53f52
Kerwin <zhouz88@uw.edu>
2018-03-27 18:14:50 -0700

    Update 358. Rearrange String k Distance Apart.java

--- src/main/358. Rearrange String k Distance Apart.java
+++ src/main/358. Rearrange String k Distance Apart.java
@@ -1,66 +1,32 @@
-//358. Rearrange String k Distance Apartimport java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-
 class Solution {
     public String rearrangeString(String s, int k) {
-        if (k <= 1) { //bug 1
-            return s;
-        }
-        StringBuilder sb = new StringBuilder();
-        int[] map = new int[26];
+        int[] counts = new int[26];
+        int[] valid = new int[26];
         for (char ch : s.toCharArray()) {
-            map[ch - 'a']++;
+            counts[ch - 'a']++;
         }
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        for (int i = 0; i < 26; i++) {
-            if (map[i] == 0) continue;
-            pq.add(new Node((char)(i + 'a'), map[i], 0));
+        char[] res = new char[s.length()];
+        for (int i = 0; i < res.length; i++) {
+            int j = nextOk(counts, valid, i, k);
+            if (j == -1) return "";
+            res[i] = (char) (j + 'a');
         }
-        
-        List<Node> coolingList = new ArrayList<>();
-        while (!pq.isEmpty() || !coolingList.isEmpty()) {
-            if (!pq.isEmpty()) {
-                Node tmp = pq.poll();
-                tmp.val --;
-                sb.append(tmp.ch);
-                tmp.cooling = k - 1;
-                updateCoolingList(coolingList, pq);
-                if (tmp.val > 0) {
-                    coolingList.add(tmp);
-                }
-            } else {
-                return "";
+        return new String(res);
+    }
+
+    private int nextOk(int[] counts, int[] valid, int index, int k) {
+        int maxIdx = -1;
+        for (int i = 0; i < counts.length; i++) {
+            if (valid[i] <= index && counts[i] > 0) {
+                if (maxIdx == -1) maxIdx = i;
+                else if (counts[i] > counts[maxIdx]) maxIdx = i;
             }
         }
-        return sb.toString();
-    }
-
-    private void updateCoolingList(List<Node> coolingList, PriorityQueue<Node> pq) {
-        for (int i = 0; i < coolingList.size();) {
-            if (coolingList.get(i).cooling == 1) {
-                pq.add(coolingList.get(i));
-                coolingList.remove(i);
-            } else {
-                coolingList.get(i).cooling--;
-                i++;
-            }
+        if (maxIdx == -1) return -1;
+        counts[maxIdx]--;
+        if (counts[maxIdx] > 0) {
+            valid[maxIdx] = index + k;
         }
+        return maxIdx;
     }
-
-    private static class Node implements Comparable<Node>{
-        int val;
-        char ch;
-        int cooling;
-        public Node(char ch, int val, int cooling) {
-            this.ch = ch;
-            this.val = val;
-            this.cooling = cooling;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return o.val - this.val;
-        }
-    }
+}
