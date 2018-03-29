4f14d76aac659b5d3f22b186cb87ca71ff694dea
Kerwin <zhouz88@uw.edu>
2018-03-29 03:59:52 -0700

    Update 802 Find Eventual Safe States.java

--- src/main/802 Find Eventual Safe States.java
+++ src/main/802 Find Eventual Safe States.java
@@ -1,53 +1,33 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<Integer> res = new ArrayList<>();
-        int n = graph.length;
-        boolean[] visited = new boolean[n];
-        int[] hasCycle = new int[n];
-        for (int i = 0; i < n; i++) {
-            visited[i] = true;
-            if (dfs(i, visited, graph, hasCycle)) {
+        int[] hasCycle = new int[graph.length];
+        // -1 cycle 0 not visite 1 no cycle;
+        for (int i = 0; i < graph.length; i++) {
+            if (!hasCycle(i, graph, hasCycle)) {
                 res.add(i);
-            } else {
-                hasCycle[i] = -1;
             }
-            visited[i] = false;
         }
-        Collections.sort(res);
         return res;
     }
 
-    private boolean dfs(int start, boolean[] visited, int[][] graph, int[] hasCycle) {
-        if (graph[start].length == 0) {
-            return true;
-        }
-
-        if (hasCycle[start] == -1) {
+    private boolean hasCycle(int start, int[][] graph, int[] hasCycle) {
+        if (hasCycle[start] == 1) {
             return false;
         }
-
-        if (hasCycle[start] == 1) {
+        if (hasCycle[start] == -1) {
             return true;
         }
-        
-
+        hasCycle[start] = -1;
         for (int i = 0; i < graph[start].length; i++) {
-            if (visited[graph[start][i]]) {
-                hasCycle[graph[start][i]] = -1;
-                return false;
+            if (hasCycle(graph[start][i], graph, hasCycle)) {
+                return true;
             }
-            visited[graph[start][i]] = true;
-            if (!dfs(graph[start][i], visited, graph, hasCycle)) {
-                hasCycle[start] = -1;
-                return false;
-            }
-            visited[graph[start][i]] = false;
         }
         hasCycle[start] = 1;
-        return true;
+        return false;
     }
 }
