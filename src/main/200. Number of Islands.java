7a3101f8eea6370a188613acebeacb1ae838e3fe
Kerwin <zhouz88@uw.edu>
2018-03-28 21:51:24 -0700

    Update 200. Number of Islands.java

--- src/main/200. Number of Islands.java
+++ src/main/200. Number of Islands.java
@@ -1,4 +1,47 @@
 class Solution {
+    void dfs(char[][] grid, int r, int c) {
+        int nr = grid.length;
+        int nc = grid[0].length;
+
+        if (r < 0 || c < 0 || r >= nr || c >= nc || grid[r][c] != '1') {
+            return;
+        }
+
+        grid[r][c] = '?';//dfs 记录搜索树的路径， 回溯法不记录路径
+        dfs(grid, r - 1, c);
+        dfs(grid, r + 1, c);
+        dfs(grid, r, c - 1);
+        dfs(grid, r, c + 1);
+    }
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int nr = grid.length;
+        int nc = grid[0].length;
+        int num_islands = 0;
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                if (grid[r][c] == '1') {
+                    ++num_islands;
+                    dfs(grid, r, c);
+                }
+            }
+        }
+
+        for (int r = 0; r < nr; ++r) {
+            for (int c = 0; c < nc; ++c) {
+                System.out.print(grid[r][c]);
+            }
+            System.out.println();
+        }
+        return num_islands;
+    }
+}
+//UNION FIND
+class Solution {
     public int numIslands(char[][] grid) {
 
         //corner case return 0 or throw Exception() ?? 
