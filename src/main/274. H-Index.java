101a476d3f1c9186080ebf8e1281abebfcd53b5a
Kerwin <zhouz88@uw.edu>
2018-03-30 15:05:24 -0700

    Update 274. H-Index.java

--- src/main/274. H-Index.java
+++ src/main/274. H-Index.java
@@ -100,16 +100,17 @@ Seen this question in a real interview before?  YesNo
 
 */
 class Solution {
- public int hIndex(int[] citations) {
-        if(citations == null || citations.length == 0) return 0;
-        int l = 0, r = citations.length;
-        int n = citations.length;
-        while (l < r){
-            int mid = l + (r - l) / 2;
-            if(citations[mid] == n - mid) return n - mid;
-            if(citations[mid] < citations.length - mid) l = mid + 1;
-            else r = mid;
+    public int hIndex(int[] citations) {
+        int len = citations.length;
+        int l = 0, r = citations.length - 1;
+        while (l <= r) {
+            int mid = ((r - l) >> 1) + l;
+            if (citations[mid] >= len - mid) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
         }
-        return n - l;
+        return len - l;
     }
 }
