3cd5f7fea6656b4ca8550f78a27800761edf0a00
Kerwin <zhouz88@uw.edu>
2018-03-30 20:23:27 -0700

    Update 48. Rotate Image.java

--- src/main/48. Rotate Image.java
+++ src/main/48. Rotate Image.java
@@ -12,3 +12,26 @@ class Solution {
         }
     }
 }
+
+//
+class Solution {
+    public void rotate(int[][] matrix) {
+        for (int j = 0; j < matrix.length; j++) {
+            int l = 0, r = matrix.length - 1;
+            while (l < r) {
+                int tmp = matrix[r][j];
+                matrix[r][j] = matrix[l][j];
+                matrix[l][j] = tmp;
+                r--;
+                l++;
+            }
+        }
+       for (int i = 0; i < matrix.length; i++) {
+            for (int j = i + 1; j < matrix[0].length; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+       }
+    }
+}
