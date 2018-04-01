5f0fc29c1d05e5ca5b15c1acadeded9128d99c17
Kerwin <zhouz88@uw.edu>
2018-04-01 00:43:16 -0700

    Update 251. Flatten 2D Vector.java

--- src/main/251. Flatten 2D Vector.java
+++ src/main/251. Flatten 2D Vector.java
@@ -1,49 +1,39 @@
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 public class Vector2D implements Iterator<Integer> {
-    List<Iterator<Integer>> list = new ArrayList<>();
-    List<List<Integer>> vec2d;
-    Iterator<Integer> itr;
-    int i = 0;
+    private Iterator<Integer> itr;
+    private List<List<Integer>> vec2d;
+    private int idx = 0;
 
     public Vector2D(List<List<Integer>> vec2d) {
         this.vec2d = vec2d;
-        for (int i = 0; i < vec2d.size(); i++) {
-            list.add(vec2d.get(i).iterator());
-        }
-        if (vec2d.size() != 0) {
-            itr = list.get(0);
-        }
     }
 
     @Override
     public Integer next() {
         if (!hasNext()) {
-            return -1;
+            return null;
         }
         return itr.next();
     }
 
     @Override
     public boolean hasNext() {
+        if (idx >= vec2d.size()) return false;
         if (itr == null) {
-            return false;}
-        if (itr .hasNext()) {
+            itr = vec2d.get(idx).iterator();
+        } 
+        if (itr.hasNext()) {
             return true;
         }
-        i++;
-        if (i == list.size()) {
-            return false;
+        idx++;
+        while (idx < vec2d.size() && vec2d.get(idx).size() == 0) {
+            idx++;
         }
-        while (i < list.size() && !list.get(i).hasNext()) {
-            i++;
-        }
-        if (i < list.size()) {
-            itr = list.get(i);
-        }
-        return i < list.size();
+        if (idx >= vec2d.size()) return false;
+        itr = vec2d.get(idx).iterator();
+        return true;
     }
 }
 
