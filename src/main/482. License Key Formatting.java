be47cb36ccbef70c784b3c5b7a28a3e5fb2387d4
Kerwin <zhouz88@uw.edu>
2018-04-03 12:40:28 -0700

    Update 482. License Key Formatting.java

--- src/main/482. License Key Formatting.java
+++ src/main/482. License Key Formatting.java
@@ -1,24 +1,19 @@
 class Solution {
     public String licenseKeyFormatting(String S, int K) {
-        int j, i = 0;
         StringBuilder sb = new StringBuilder();
-        for (j = S.length() - 1; j >= 0; j--) {
-            if (S.charAt(j) == '-') {
-                continue;
-            }
-            sb.append(Character.toUpperCase(S.charAt(j)));
-            i++;
-            if (i == K) {
+        int idx = 0;
+        for (int i = S.length() - 1; i >= 0; i--) {
+            if (S.charAt(i) == '-') continue;
+            char cur = Character.toUpperCase(S.charAt(i));
+            sb.append(cur);
+            if (++idx == K) {
                 sb.append('-');
-                i = 0;
-            } 
+                idx = 0;
+            }
         }
-        if (sb.length() == 0) { //bug 1
-            return sb.toString();
-        }
-        if (sb.charAt(sb.length() - 1) == '-') {
+        if (sb.length() > 0 && sb.charAt(sb.length() - 1) == '-') {
             sb.deleteCharAt(sb.length() - 1);
-        }
+        }//关键点！！！
         return sb.reverse().toString();
     }
 }
