6e3a28953d7de23bea0f071d594656658b5ad85f
Kerwin <zhouz88@uw.edu>
2018-04-02 17:14:49 -0700

    Update 562. Longest Line of Consecutive One in Matrix.java

--- src/main/562. Longest Line of Consecutive One in Matrix.java
+++ src/main/562. Longest Line of Consecutive One in Matrix.java
@@ -1,33 +1,49 @@
-import java.util.Arrays;
-
 class Solution {
     public int longestLine(int[][] M) {
-        if (M == null || M.length==0||M[0].length ==0) {
+        if (M == null || M.length == 0 || M[0].length == 0) {
             return 0;
         }
         int m = M.length, n = M[0].length;
-        final int[][] directions = {{0, 1},{1, 1},{1, 0},{1, -1}};
-        int[][][] dp = new int[4][m][n];
+        int[][] up = new int[m][n];
+        int[][] left = new int[m][n];
+        int[][] upLeft = new int[m][n];
+        int[][] upRight = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            left[i][0] = (M[i][0] == 1 ? 1 : 0);
+            upLeft[i][0] = left[i][0];
+            upRight[i][n - 1] = (M[i][n - 1] == 1 ? 1 : 0);
+        }
+        for (int j = 0; j < n; j++) {
+            up[0][j] = (M[0][j] == 1 ? 1 : 0);
+            upLeft[0][j] = up[0][j];
+            upRight[0][j] = up[0][j];
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                up[i][j] = (M[i][j] == 0 ? 0 : up[i - 1][j] + 1);
+            }
+        }
+        for (int j = 1; j < n; j++) {
+            for (int i = 0; i < m; i++) {
+                left[i][j] = (M[i][j] == 0 ? 0 : left[i][j - 1] + 1);
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j< n; j++) {
+                upLeft[i][j] = (M[i][j] == 0 ? 0 : upLeft[i - 1][j - 1] + 1);
+            }
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = n - 2; j >= 0; j--) {
+                upRight[i][j] = (M[i][j] == 0 ? 0 : upRight[i - 1][j + 1] + 1);
+            }
+        }
         int max = 0;
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (M[i][j] == 1) {
-                    int k = 0;
-                    for (int[] dir : directions) {
-                        max = Math.max(max, check(M, i, j, dir, dp[k++]));
-                    }
-                }
+            for (int j = 0; j< n; j++) {
+                max = Math.max(max, Math.max(Math.max(up[i][j], left[i][j]), Math.max(upLeft[i][j], upRight[i][j])));
             }
         }
         return max;
-    }
-
-    private int check(int[][] M, int i, int j, int[] dir, int[][] dp) {
-        if (i>=0&&i<M.length&&j>=0&&j<M[0].length&&M[i][j]==1) {
-            if(dp[i][j]!=0) return dp[i][j];
-            else return dp[i][j] = 1 + check(M, i + dir[0], j + dir[1], dir, dp);
-        } else {
-            return 0;
-        }
     }
 }
