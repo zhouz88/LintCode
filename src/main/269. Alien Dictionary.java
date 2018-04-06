eb5471364fce7107f9c91e36a1fd6768c6a44b65
Kerwin <zhouz88@uw.edu>
2018-04-06 03:20:01 -0700

    Update 269. Alien Dictionary.java

--- src/main/269. Alien Dictionary.java
+++ src/main/269. Alien Dictionary.java
@@ -2,65 +2,70 @@ import java.util.*;
 
 class Solution {
     public String alienOrder(String[] words) {
-        StringBuilder sb =  new StringBuilder();
-        Set<Character>[] graph = new HashSet[26];
+        //1 get edges;
+
+        //2 build adjList and set of characters and indegrees table
+
+        //3 do bfs;
+
+        //4, return result;
+        List<char[]> edges = new ArrayList<>();
         Set<Character> set = new HashSet<>();
+        buildEdges(edges, words);
+        buildSet(set, words);
         
-        for (int i = 0; i < 26; i++) graph[i] = new HashSet<>();
-        buildGraphAndSet(words, graph, set);
-        Map<Character, Integer> indegree = new HashMap<>();
-        buildeIndegree(indegree, graph);
+        List<Character>[] adjList = new ArrayList[26];
+        for (int i = 0; i < 26; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        int[] indegrees = new int[26];
+        for (char[] e : edges) {
+            adjList[e[0] - 'a'].add(e[1]);
+            indegrees[e[1] - 'a']++;
+        }
+        StringBuilder sb = new StringBuilder();
         Queue<Character> q = new LinkedList<>();
-        for (Character ch : set) {
-            // System.out.println(ch);
-            if (!indegree.containsKey(ch)) {
-                q.add(ch);
+        for (int i = 0; i < 26; i++) {
+            if (set.contains((char)(i+'a')) && indegrees[i] == 0) {
+                q.add((char)(i+'a'));
             }
         }
         while (!q.isEmpty()) {
             Character node = q.poll();
             sb.append(node);
-            for (Character ch : graph[node - 'a']) {
-               int number = indegree.get(ch);
-               if (number == 1) {
-                   indegree.remove(ch);
-                   q.add(ch);
-               } else {
-                   number--;
-                   indegree.put(ch, number);
-               }
-            }
-        }
-        if (sb.length() != set.size()) {
-            return "";
-        }
-        return sb.toString();
-    }
-
-    private void buildeIndegree(Map<Character, Integer> indegree, Set<Character>[] graph) {
-        for (int i = 0; i < graph.length; i++) {
-            if (graph[i].size() != 0) {
-                for (Character ch : graph[i]) {
-                    indegree.put(ch, indegree.getOrDefault(ch, 0) + 1);
+            for (int z = 0; z < adjList[node - 'a'].size(); z++) {
+                Character next = adjList[node - 'a'].get(z);
+                if (indegrees[next - 'a'] > 1) {
+                    indegrees[next - 'a']--;
+                } else if (indegrees[next - 'a'] == 1) {
+                    indegrees[next - 'a']--;
+                    q.add(next);
                 }
             }
         }
+        return sb.toString().length() == set.size() ? sb.toString() : "";
     }
 
-    private void buildGraphAndSet(String[] words, Set<Character>[] graph, Set<Character> set) {
+    private void buildSet(Set<Character> set, String[] words) {
+        for (String k : words) {
+            for (char ch : k.toCharArray()) {
+                set.add(ch);
+            }
+        }
+    }
+
+    private void buildEdges(List<char[]> edges, String[] words) {
         for (int i = 0; i < words.length - 1; i++) {
             String start = words[i];
-            String end = words[i + 1];
-            for (int j = 0; j < Math.min(start.length(), end.length()); j++) {
-                if (start.charAt(j) != end.charAt(j)) {
-                    graph[start.charAt(j) - 'a'].add(end.charAt(j));
+            String second = words[i + 1];
+            for (int l=0, r=0; l<start.length()&&r<second.length();) {
+                if (start.charAt(l) == second.charAt(r)) {
+                    l++;
+                    r++;
+                }else{
+                    edges.add(new char[]{start.charAt(l), second.charAt(r)});
                     break;
                 }
-            }
-        }
-        for (int i = 0; i < words.length; i++) {
-            for (char ch : words[i].toCharArray()) {
-                set.add(ch);
             }
         }
     }
