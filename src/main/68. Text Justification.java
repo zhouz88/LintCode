ea7bc8ce28271084d9ae7858eec0b3de1be6dd66
Kerwin <zhouz88@uw.edu>
2018-04-05 23:09:54 -0700

    Update 68. Text Justification.java

--- src/main/68. Text Justification.java
+++ src/main/68. Text Justification.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> res = new ArrayList<>();
-        for (int i = 0; i < words.length; ) {
+        for (int i = 0; i < words.length;) {
             int j = i;
-            int cnt = words[i].length();
-            while (j + 1 < words.length && cnt + words[j + 1].length() + 1 <= maxWidth) {
-                cnt += 1 + words[++j].length();
-            }
+            int total = words[j].length();
             StringBuilder sb = new StringBuilder();
-            sb.append(words[i]);
+            sb.append(words[j]);
+            while (j + 1 < words.length && total + words[j + 1].length() + 1 <= maxWidth) {
+                total += 1 + words[++j].length();
+            }
             if (j == i || j == words.length - 1) {
-                for (int k = i + 1 ; k <= j; k++) {
+                for (int k = i + 1; k <= j; k++) {
                     sb.append(" ");
                     sb.append(words[k]);
                 }
@@ -18,16 +18,15 @@ class Solution {
                     sb.append(" ");
                 }
             } else {
-                int n = (maxWidth - cnt)/(j - i);
-                int r = (maxWidth - cnt)%(j - i);
+                int n = (maxWidth - total)/(j - i);
+                int m = (maxWidth - total)%(j - i);
                 for (int k = i + 1; k <= j; k++) {
                     sb.append(" ");
-                    for (int t = 0; t < n; t++) {
+                    for (int z = 0; z < n; z++) {
                         sb.append(" ");
                     }
-                    if (r > 0) { //TWO POINTERS
+                    if (m-- > 0) {
                         sb.append(" ");
-                        r--;
                     }
                     sb.append(words[k]);
                 }
