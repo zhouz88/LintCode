8e8276f89611bae6c00e5864bc3c446310db7946
Kerwin <zhouz88@uw.edu>
2018-04-06 11:24:35 -0700

    Update 399. Evaluate Division.java

--- src/main/399. Evaluate Division.java
+++ src/main/399. Evaluate Division.java
@@ -2,67 +2,59 @@ import java.util.*;
 
 class Solution {
     public double[] calcEquation(String[][] equations, double[] values, String[][] queries) {
-        int idx = 0;
-        Map<String, Integer> map = new HashMap<>();//used for mapping strings to distinct numbmer;
-        for (String[] k : equations) {
-            if (!map.containsKey(k[0])) {
-                map.put(k[0], idx++);
-            }
-            if (!map.containsKey(k[1])) {
-                map.put(k[1], idx++);
-            }
-        }
-        Double[][] graph = new Double[idx][idx];
-        idx = 0;
-        for (String[] k : equations) {
-            int first = map.get(k[0]);
-            int second = map.get(k[1]);
-            graph[first][second] = values[idx];
-            if (values[idx] == 0) continue;
-            graph[second][first] = 1.0 / values[idx];
-            idx++;
+        Map<String, List<GraphNode>> map = new HashMap<>();
+        for (int i = 0; i < equations.length; i++) {
+            String[] str = equations[i];
+            map.putIfAbsent(str[0], new ArrayList<>());
+            map.putIfAbsent(str[1], new ArrayList<>());
+            map.get(str[0]).add(new GraphNode(str[1], values[i]));
+            map.get(str[1]).add(new GraphNode(str[0], 1.0/values[i]));
         }
         double[] res = new double[queries.length];
-        for (idx = 0; idx < queries.length; idx++) {
-            res[idx] = bfs(graph, queries[idx], map);
+        for (int i = 0; i < queries.length; i++) {
+            res[i] = bfs(map, queries[i][0], queries[i][1]);
         }
         return res;
     }
 
-    private double bfs(Double[][] graph, String[] query, Map<String, Integer> map) {
-        if (!map.containsKey(query[1]) || !map.containsKey(query[0])) {
-            return -1.0;
+    private double bfs(Map<String, List<GraphNode>> map, String start, String end) {
+        if (!map.containsKey(start)) { //bug 1
+            return -1;
         }
-        
-        int end = map.get(query[1]);
-        int start = map.get(query[0]);
-        
-        Queue<Integer> q = new LinkedList<>();
-        Queue<Double> doubleValues = new LinkedList<>();
+        Queue<String> q = new LinkedList<>();
+        Queue<Double> values = new LinkedList<>();
+        Set<String> visited = new HashSet<>();
         q.add(start);
-        doubleValues.add(1.0);
-        
-        Set<Integer> visited = new HashSet<>();
+        values.add(1.0);
         visited.add(start);
-        
         while (!q.isEmpty()) {
             int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int node = q.poll();
-                double value = doubleValues.poll();
-                if (end == node) {
+            for (int z = 0; z < size; z++) {
+                String node = q.poll();
+                double value = values.poll();
+                if (node.equals(end)) {
                     return value;
                 }
-                for (int j = 0; j < graph[node].length; j++) {
-                    if (graph[node][j] != null && !visited.contains(j)) {
-                        double newValue = graph[node][j] * value;
-                        q.add(j);
-                        doubleValues.add(newValue);
-                        visited.add(j);
+                if (map.get(node) == null) continue; //bug2 
+                for (int i = 0; i < map.get(node).size(); i++) {
+                    GraphNode next = map.get(node).get(i);
+                    if (!visited.contains(next.cur)) {
+                        q.add(next.cur);
+                        values.add(value * next.val);
+                        visited.add(next.cur); //bug3
                     }
                 }
             }
         }
-        return -1.0;
+        return -1;
+    }
+
+    private static class GraphNode {
+        String cur;
+        double val;
+        public GraphNode(String name, double b) {
+            this.cur = name;
+            this.val = b;
+        }
     }
 }
