2e8508f73e6d5397b3b5ee002ef3841abb9335c8
Kerwin <zhouz88@uw.edu>
2018-04-07 18:11:13 -0700

    Update 529. Minesweeper.java

--- src/main/529. Minesweeper.java
+++ src/main/529. Minesweeper.java
@@ -45,3 +45,67 @@ class Solution {
         return cnt;
     }
 }
+
+//bfs
+class Solution {
+    public char[][] updateBoard(char[][] board, int[] click) {
+        //'E' 'M' 'B' '1'
+        switch (board[click[0]][click[1]]) {
+            case 'B':
+                return board;
+            case 'E':
+                bfs(board, click);
+                return board;
+            case 'M':
+                board[click[0]][click[1]] = 'X';
+                return board;
+            default:
+                return board;
+        }
+    }
+
+    private static final int[][] DIRECTIONS = {{1, 0},{-1, 0},{0, 1},{0, -1}, {1, -1},{-1, 1},{1, 1},{-1, -1}};//八个方向bfs;
+    
+    private void bfs(char[][] board, int[] click) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(click);
+        int cnt = cnt(board, click[0], click[1]);
+        if (cnt > 0) {
+            board[click[0]][click[1]] = (char) (cnt + '0');
+            return ;
+        } else {
+            board[click[0]][click[1]] = 'B';
+        }
+        while (!q.isEmpty()) {
+            int[] node = q.poll();
+            for (int[] dir : DIRECTIONS) {
+                int x = node[0] + dir[0];
+                int y = node[1] + dir[1];
+                if (x>=0&&y>=0&&x<board.length&&y<board[0].length&&board[x][y]=='E') {
+                    int count = cnt(board, x, y);
+                    if (count > 0) {
+                        board[x][y] = (char) (count + '0');
+                    } else {
+                        board[x][y] = 'B';
+                        q.add(new int[]{x, y});
+                    }
+                }
+            }
+        }
+    }
+
+    private int cnt (char[][] grid, int x, int y) {
+        int cnt = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (j == i && j == 0) continue;
+                int a = x + i;
+                int b = y + j;
+                if (a>=0&&a<grid.length&&b>=0&&b<grid[0].length&&grid[a][b]=='M'){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
