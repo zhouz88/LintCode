399d3e0d0ccfa6a30f1a2e6a2dfcd4101f771908
Kerwin <zhouz88@uw.edu>
2018-04-09 19:38:03 -0700

    Update 524. Longest Word in Dictionary through Deleting.java

--- src/main/524. Longest Word in Dictionary through Deleting.java
+++ src/main/524. Longest Word in Dictionary through Deleting.java
@@ -40,3 +40,41 @@ class Solution {
         return 0;
     }
 }
+
+//
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Solution {
+    public String findLongestWord(String s, List<String> d) {
+        s = " " + s;
+        int n = s.length();
+        int[] lastId = new int[26];
+        int[][] map = new int[n][26];
+        for (int i = 1; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            for (int j = lastId[cur - 'a']; j < i; j++) {
+                map[j][cur - 'a'] = i;
+            }
+            lastId[cur - 'a'] = i;
+        }
+        Collections.sort(d, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) { 
+                return Integer.compare(o2.length(), o1.length()) == 0 ? 
+                        o1.compareTo(o2): Integer.compare(o2.length(), o1.length());
+            }
+        });
+        for (String k : d) {
+            int idx = 0;
+            for (int i = 0;i < k.length(); i++) {
+                idx = map[idx][k.charAt(i) - 'a'];
+                if (idx == 0) break;
+                if (idx >= map.length) break;
+                if (i == k.length() - 1) return k;
+            }
+        }
+        return "";
+    }
+}
