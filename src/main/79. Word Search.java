68bcb0e2acbe3f6b0df7629b48dcea5e1854d6e1
Kerwin <zhouz88@uw.edu>
2018-04-10 13:37:40 -0700

    Update 79. Word Search.java

--- src/main/79. Word Search.java
+++ src/main/79. Word Search.java
@@ -1,31 +1,26 @@
 class Solution {
-    
-    //https://stackoverflow.com/questions/1294720/whats-the-difference-between-backtracking-and-depth-first-search
     public boolean exist(char[][] board, String word) {
-        //corner case
-         if (board == null || board.length == 0 || board[0].length == 0 || word == null) {
-             return false;
-         }
-         if (word.equals("")) {
-             return true;
-         }
-         for (int i = 0; i < board.length; i++) {
-             for (int j = 0; j < board[0].length; j++) {
-                 if (dfs(board, word, i, j, 0)) {
-                     return true;
-                 }
-             }
-         }
-         return false;
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+        int m = board.length, n = board[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dfs(board, i, j, word, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     
-    private final static int[][] DIRECTIONS = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    
-    private boolean dfs(char[][] board, String word, int i, int j, int start) {
-        if (start == word.length()) {
+    private static final int[][] DIRECTIONS = {{1,0},{0,1},{-1,0},{0,-1}};
+
+    private boolean dfs(char[][] board, int i, int j, String word, int start) {
+        if (start == word.length()) {//if word == ""
             return true;
         }
-        if (board[i][j] != word.charAt(start)) {
+        if (word.charAt(start) != board[i][j]) {
             return false;
         }
         if (start == word.length() - 1) {
@@ -33,14 +28,16 @@ class Solution {
         }
         char tmp = board[i][j];
         board[i][j] = '*';
-        boolean found = false;
         for (int[] dir : DIRECTIONS) {
-            int x = i + dir[0];
-            int y = j + dir[1];
-            if (x<0||y<0||x>=board.length||y>=board[0].length||board[x][y]=='*') continue;
-            found = found || dfs(board, word, x, y, start + 1);
+            int x = dir[0] + i;
+            int y = dir[1] + j;
+            if (x>=0&&y>=0&&x<board.length&&y<board[0].length&&board[x][y]!='*') {
+                if (dfs(board, x, y, word, start + 1)) {
+                    return true;
+                }
+            }
         }
         board[i][j] = tmp;
-        return found;
+        return false;
     }
 }
