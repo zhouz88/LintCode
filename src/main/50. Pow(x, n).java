4991cd0ffa921e3698dafbd0dbffea78c437dc1f
Kerwin <zhouz88@uw.edu>
2018-04-12 00:14:15 -0700

    Update 50. Pow(x, n).java

--- src/main/50. Pow(x, n).java
+++ src/main/50. Pow(x, n).java
@@ -1,29 +1,17 @@
 class Solution {
-    public double myPow(double x, int N) { 
-        long n = (long)N;//第一步 考虑越界！！！！！！！！！！
-        if (n == 0) {
-            return 1;
+    public double myPow(double x, int n) {
+        double sign = n < 0 ? -1 : 1;
+        long N = Math.abs((long)n);
+        double ans = 1.0, tmp = x;
+        
+        while (N != 0) {
+            if (N % 2 == 1) {
+                ans *= tmp;
+            }
+            tmp *= tmp;
+            N/= 2;
         }
         
-        long len = 1;
-        double res = 1.0;
-        double pre = x;
-        int sign = 1;
-        if (n < 0) {
-            n = -n;
-            sign = -1;
-        }
-        while (n > 0) {
-            x *= x;
-            len *= 2;
-            long m = n % len;
-            n -= m;
-            if (m != 0) {
-                res *= pre;
-            }
-            pre = x;
-        }
-
-        return sign == 1 ?res : 1.0/res;
+        return sign == -1 ? 1/ans : ans;
     }
 }
