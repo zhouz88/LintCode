7e15360e04a4601e221ef473ce1f3f936e602b9c
Kerwin <zhouz88@uw.edu>
2018-04-14 15:56:16 -0700

    Update 408. Valid Word Abbreviation.java

--- src/main/408. Valid Word Abbreviation.java
+++ src/main/408. Valid Word Abbreviation.java
@@ -23,3 +23,29 @@ class Solution {
         return i == word.length() && j == abbr.length();
     }
 }
+
+
+//
+class Solution {
+    public boolean validWordAbbreviation(String word, String abbr) {
+        int i = 0, j = 0;
+        while (i < word.length() && j < abbr.length()) {
+            if (Character.isLetter(abbr.charAt(j))) {
+                if (word.charAt(i) != abbr.charAt(j)) {
+                    return false;
+                }
+                i++;
+                j++;
+            } else {
+                int temp = abbr.charAt(j)- '0';
+                if (temp == 0) return false;
+                while (j + 1 < abbr.length() && Character.isDigit(abbr.charAt(j + 1))) {
+                    temp = temp * 10 + abbr.charAt(++j) - '0';
+                }
+                j++;
+                i += temp;
+            }
+        }
+        return i == word.length() && j == abbr.length();
+    }
+}
