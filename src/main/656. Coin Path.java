b995686f41a067b3f5687988072d09824dd6148c
Kerwin <zhouz88@uw.edu>
2018-04-14 11:40:28 -0700

    Update 656. Coin Path.java

--- src/main/656. Coin Path.java
+++ src/main/656. Coin Path.java
@@ -1,43 +1,38 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public List<Integer> cheapestJump(int[] A, int B) {
-        List<Integer> ret = new ArrayList<>();
         int[] dp = new int[A.length];
+        Arrays.fill(dp, Integer.MAX_VALUE);
         dp[0] = A[0];
         for (int i = 1; i < A.length; i++) {
-            dp[i] = Integer.MAX_VALUE;
-            if (A[i] == -1) continue;
-            for (int j = Math.max(0, i - B); j < i; j++) {
-                if (A[j] == -1) continue;
-                dp[i] = Math.min(dp[j] + A[i], dp[i]);
+            if (A[i] == -1)continue;
+            for (int j = Math.max(i - B, 0); j < i; j++) {
+                if (dp[j] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i], dp[j] + A[i]);
+                }
             }
         }
-        if (dp[A.length - 1] == Integer.MAX_VALUE) {
-            return ret;
-        }
+        if (dp[A.length - 1] == Integer.MAX_VALUE) return new ArrayList<>();
         List<Integer> list = new ArrayList<>();
         list.add(1);
-        dfs(A, 0, dp, list, B, ret);
-        return ret;
+        dfs(dp, A, list, 0, B);
+        return list;
     }
 
-    private void dfs(int[] A, int start, int[] dp, List<Integer> res, int B, List<Integer> ret) {
-        if (ret.size() != 0) {
-            return;
+    private boolean dfs(int[] dp, int[] A, List<Integer> list, int start, int B) {
+        if (start == A.length - 1) {
+            return true;
         }
-        if (ret.size() == 0 && start == A.length - 1) {
-            ret.addAll(new ArrayList<>(res));
-            return;
-        }
-        for (int i = start + 1; i <= Math.min(start + B, A.length - 1); i++) {
-            if (dp[i] != Integer.MAX_VALUE  && dp[start] + A[i] == dp[i]) {
-                res.add(i + 1);
-                dfs(A, i, dp, res, B, ret);
-                res.remove(res.size() - 1);
+        for (int i = start + 1 ; i <= start + B && i < A.length; i++) {
+            if (dp[i] == dp[start] + A[i]) {
+                list.add(i + 1);
+                if (dfs(dp, A, list, i, B)) {
+                    return true;
+                }
+                list.remove(list.size() - 1);
             }
         }
+        return false;
     }
 }
