b3ef4892ef6ad0fc80592b3046936ca4b319a964
Kerwin <zhouz88@uw.edu>
2018-04-14 16:07:42 -0700

    Update 544. Output Contest Matches.java

--- src/main/544. Output Contest Matches.java
+++ src/main/544. Output Contest Matches.java
@@ -1,28 +1,36 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
-    public String findContestMatch(int N) {
+    public String findContestMatch(int n) {
         TreeNode root = new TreeNode(1);
         Queue<TreeNode> q = new LinkedList<>();
         q.add(root);
-        int level = 1;
-        int n = (int)(Math.log(N)/Math.log(2) + 0.5);
-        while (!q.isEmpty() && level <= n) {
+        int lev = 1;
+        while (!q.isEmpty()) {
             int size = q.size();
-            for (int z = 0; z < size; z++) {
+            lev <<= 1;
+            for (int z = 0; z < size; z++) {//care!!!
                 TreeNode node = q.poll();
                 node.left = new TreeNode(node.val);
-                node.right = new TreeNode((1 << level)  + 1 - node.val);
+                node.right = new TreeNode(lev + 1 - node.val);
                 q.add(node.left);
                 q.add(node.right);
             }
-            level++;
+            if (lev == n) break;
         }
         return dfs(root);
     }
 
     private String dfs(TreeNode root) {
-        if (root.left == null && root.right == null) {
-            return root.val+"";
+        if (root == null) {
+            return "";
         }
-        return "(" +dfs(root.left) +"," + dfs(root.right)+")";
+        if (root.left == null && root.right == null) {
+            return root.val + "";
+        }
+        String l = dfs(root.left);
+        String r = dfs(root.right);
+        return "(" + l + "," + r + ")";
     }
 }
