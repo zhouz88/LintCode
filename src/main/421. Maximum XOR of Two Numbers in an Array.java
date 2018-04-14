97d0349fc5c18124cecd422f41515aa6a254da1f
Kerwin <zhouz88@uw.edu>
2018-04-14 15:41:26 -0700

    Update 421. Maximum XOR of Two Numbers in an Array.java

--- src/main/421. Maximum XOR of Two Numbers in an Array.java
+++ src/main/421. Maximum XOR of Two Numbers in an Array.java
@@ -39,3 +39,30 @@ class Solution {
         return max;
     }
 }
+//
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int findMaximumXOR(int[] nums) {
+        int max = 0;
+        int mask = 0;
+        for (int i = 30; i >= 0; i--) {
+            int can = max | 1 << i;
+            mask |= 1 << i;
+            Set<Integer> set = new HashSet<>();
+            for (int num : nums) {
+                set.add(num & mask);
+            }
+            for (int prefix : set) {
+                if (set.contains(can ^ prefix)) {
+                    //if prefix == can ^ prefix then can = 0;
+                    //because can != 0 so can ^ prefix != prefix
+                    max = can;
+                    break;
+                }
+            }
+        }
+        return max;
+    }
+}
