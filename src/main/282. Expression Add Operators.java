c6e3e04194c6fc0a6891739f6fd700e9bb6fb4b6
Kerwin <zhouz88@uw.edu>
2019-06-23 20:48:04 -0700

    Update 282. Expression Add Operators.java

--- src/main/282. Expression Add Operators.java
+++ src/main/282. Expression Add Operators.java
@@ -60,3 +60,54 @@ class Solution {
         }
     }
 }
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> res = new ArrayList<>();
+        if (num == null || num.length() == 0) {
+            return res;
+        }
+        dfs(num, 0, target, new StringBuilder(), 0, 0, res);
+        return res;
+    }
+
+    private void dfs(String num, int start, int target, StringBuilder stringBuilder, long sum, long pre,List<String> res) {
+        if (start == 0) {
+            long temp = 0;
+            for (int i = start; i < num.length(); i++) {
+                temp = temp * 10 + num.charAt(i) - '0';
+                if (i > start && num.charAt(start) == '0') break;
+                int len = stringBuilder.length();
+                stringBuilder.append(temp);
+                dfs(num, i + 1, target, stringBuilder, sum, temp, res);
+                stringBuilder.setLength(len);
+            }
+            return;
+        }
+        if (start == num.length()) {
+            sum += pre;
+            if (sum == (long) target) {
+                res.add(stringBuilder.toString());
+            }
+            return;
+        }
+        long temp = 0;
+        for (int i = start; i < num.length(); i++) {
+            temp = temp * 10 + num.charAt(i) - '0';
+            if (i > start && num.charAt(start) == '0') break;
+            int len = stringBuilder.length();
+            stringBuilder.append("+" + temp);
+            dfs(num, i + 1, target, stringBuilder, sum + pre, temp, res);
+            stringBuilder.setLength(len);
+            stringBuilder.append("-" + temp);
+            dfs(num, i + 1, target, stringBuilder, sum + pre, -temp, res);
+            stringBuilder.setLength(len);
+            stringBuilder.append("*" + temp);
+            dfs(num, i + 1, target, stringBuilder, sum, pre * temp, res);
+            stringBuilder.setLength(len);
+        }
+    }
+}
