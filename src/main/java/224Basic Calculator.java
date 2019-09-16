f0277c7f78508575fc2bded2982367deaab9ef48
Kerwin <zhouz88@uw.edu>
2019-09-15 21:16:00 -0700

    Update 224Basic Calculator.java

--- src/main/java/224Basic Calculator.java
+++ src/main/java/224Basic Calculator.java
@@ -1,79 +1,41 @@
-import java.util.Stack;
-
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> stack = new Stack<>();
-        int i, sum = 0;
-        char sign = '+';
-        stack.add(1);
-        for (i = 0; i < s.length(); i++) {
-            switch (s.charAt(i)) {
-                case '+':
-                    sign = '+';
-                    break;
-                case '-':
-                    sign = '-';
-                    break;
+        // Integer, char '(', char '+', char '-';
+        ArrayDeque<Object> stk = new ArrayDeque<>();
+        char[] t = ("(" + s + ")").toCharArray();
+        for (int i = 0; i < t.length; i++) {
+            switch (t[i]) {
                 case ' ':
                     break;
+                case '+':
+                case '-':
                 case '(':
-                    int t = sign == '+' ? 1 : -1;
-                    stack.add(stack.peek()*t);
+                    stk.add(t[i]);
                     break;
                 case ')':
-                    stack.pop();
+                    int sum = 0;
+                    while (!stk.peekLast().equals('(')) {
+                        sum += (int) stk.pollLast();
+                    }
+                    stk.pollLast();
+                    stk.add(getPossibleSign(stk, sum));
                     break;
                 default:
-                    int tmp = s.charAt(i) - '0';
-                    if (i > 0 && s.charAt(i - 1) == '(') {
-                        sign = '+';
+                    int temp = t[i] - '0';
+                    while (i + 1 < t.length && Character.isDigit(t[i + 1])) {
+                        temp = 10 * temp + t[++i] - '0';
                     }
-                    while (i + 1 < s.length() && Character.isDigit(s.charAt(i + 1))) {
-                        tmp = 10 * tmp + s.charAt(++i) - '0';
-                    }
-                    sum += stack.peek() * tmp * (sign == '+' ? 1 : -1);
-                    break;
+                    stk.add(getPossibleSign(stk, temp));
             }
         }
-        return sum;
+        return (int) stk.pollLast();
     }
-}
 
-import java.util.ArrayDeque;
-
-class Solution {
-    public int calculate(String s) {
-        int sum = 0;
-        ArrayDeque<Integer> dq = new ArrayDeque<>();
-        dq.addLast(1);
-        int sign = 1;
-        char[] t = s.toCharArray();
-        for (int i = 0; i < t.length; i++) {
-            if (t[i] == ' ') {
-                continue;
-            } else if (Character.isDigit(t[i])) {
-                int j = i - 1;
-                while (j >= 0 && t[j] == ' ') {
-                    j--;
-                }
-                if (j >= 0 && t[j] == '(') {
-                    sign = 1;
-                }
-                int temp = t[i] - '0';
-                while (i + 1 < t.length && Character.isDigit(t[i + 1])) {
-                    temp = 10 * temp + t[++i] - '0';
-                }
-                sum += dq.peekLast() * sign * temp;
-            } else if (t[i] == '+') {
-                sign = 1;
-            } else if (t[i] == '-') {
-                sign = -1;
-            } else if (t[i] == '(') {
-                dq.add(dq.peekLast() * sign);
-            } else {
-                dq.pollLast();
-            }
+    private int getPossibleSign(ArrayDeque<Object> dq, int temp) {
+        if (!dq.isEmpty() && (dq.peekLast().equals('+') || dq.peekLast().equals('-'))) {
+            char ch = (char) dq.pollLast();
+            temp = (ch == '+' ? temp : -temp);
         }
-        return sum;
+        return temp;
     }
 }
