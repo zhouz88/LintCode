bd29fbde5f351e720f4ef590a241d1e8dbd95e9f
Kerwin <zhouz88@uw.edu>
2019-09-16 00:22:17 -0700

    Update 212. Word Search II.java

--- src/main/212. Word Search II.java
+++ src/main/212. Word Search II.java
@@ -2,74 +2,75 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Solution {
+    private static final int[][] DIRECTIONS = {{1, 0},{0, 1},{-1, 0},{0, -1}};
+    
     public List<String> findWords(char[][] board, String[] words) {
-        List<String> res = new ArrayList<>();
-        if (board == null || board.length == 0 || board[0].length  == 0) {
-            return res;
+        if (board == null || board.length == 0 || board[0].length == 0) {
+            return new ArrayList<>();
         }
-        TrieNode root = new TrieNode();
-        buildTrie(words, root);
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                dfs(res, board, root, i, j);
+        List<String> res = new ArrayList<>();
+        TrieNode root = buildTrie(words);
+        int m = board.length, n = board[0].length;
+        
+        //check empty string;
+        if (root.word != null) {
+            res.add(root.word);
+            root.word = null;
+        }
+        
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (root.nodes[board[i][j] - 'a'] != null) {
+                    char temp = board[i][j];
+                    if (root.nodes[board[i][j] - 'a'].word != null) {
+                        res.add(root.nodes[board[i][j] - 'a'].word);
+                        root.nodes[board[i][j] - 'a'].word = null;
+                    }
+                    board[i][j] = '*';
+                    dfs(i, j, root.nodes[temp - 'a'], board, m, n, res);
+                    board[i][j] = temp;
+                }
             }
         }
         return res;
     }
 
-    private static int[][] DIRECTIONS = {{1, 0},{0, 1},{-1, 0},{0, -1}};
-
-    private void dfs(List<String> res, char[][] board, TrieNode root, int i, int j) {
-        // if (root.word != null) { // to avoid "" in the dic
-        //     res.add(root.word);
-        //     root.word = null;
-        // }
-        if (root.map[board[i][j] - 'a'] == null) {
-            return;
-        }
-        if (root.map[board[i][j] - 'a'].word != null) {
-            res.add(root.map[board[i][j] - 'a'].word);
-            root.map[board[i][j] - 'a'].word = null;
-        }
-        char tmp = board[i][j];
-        board[i][j] = '*';
-        for (int[] dir :DIRECTIONS) {
-            int x = dir[0] + i;
-            int y = dir[1] + j;
-            if (x>=0&&y>=0&&x<board.length&&y<board[0].length&&board[x][y]!='*') {
-                dfs(res, board, root.map[tmp - 'a'], x, y);
-            }
-        }
-        board[i][j] = tmp;
-    }
-
-    private void buildTrie(String[] words, TrieNode root) {
-        for (int i = 0; i < words.length; i++) {
-            TrieNode node = root;
-            for (char ch : words[i].toCharArray()) {
-                if (node.map[ch - 'a'] == null) {
-                    node.map[ch - 'a'] = new TrieNode();
+    private void dfs(int i, int j, TrieNode node, char[][] board, int m, int n, List<String> res) {
+        for (int[] dir : DIRECTIONS) {
+            int nx = dir[0] + i;
+            int ny = dir[1] + j;
+            if (nx>=0&&ny>=0&&nx<m&&ny<n&&board[nx][ny]!='*'
+                    &&node.nodes[board[nx][ny] - 'a']!=null) {
+                if (node.nodes[board[nx][ny] - 'a'].word != null) {
+                    res.add(node.nodes[board[nx][ny] - 'a'].word);
+                    node.nodes[board[nx][ny] - 'a'].word = null;
                 }
-                node = node.map[ch - 'a'];
+                char temp = board[nx][ny];
+                board[nx][ny] = '*';
+                dfs(nx, ny, node.nodes[temp - 'a'], board, m, n, res);
+                board[nx][ny] = temp;
             }
-            node.word = words[i];
         }
     }
 
-    private static class TrieNode{
-        TrieNode[] map;
-        String word;
-        public TrieNode(){
-            this.map = new TrieNode[26];
-            word = null;
+
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
+        for (String k : words) {
+            TrieNode node = root;
+            for (char ch : k.toCharArray()) {
+                if (node.nodes[ch - 'a'] == null) {
+                    node.nodes[ch - 'a'] = new TrieNode();
+                }
+                node = node.nodes[ch - 'a'];
+            }
+            node.word = k;
         }
+        return root;
     }
 
-    public static void main(String[] args) {
-        char[][] board = new char[1][2];
-        board[0][0] = 'a';
-        board[0][1] = 'b';
-        String[] words = {"ab"};
-        System.out.println(new Solution().findWords(board, words));
+    private static final class TrieNode {
+        TrieNode[] nodes = new TrieNode[26];
+        String word = null;
     }
 }
