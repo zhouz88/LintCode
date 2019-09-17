67d47d947db535445e5c1f52710a348a9261ae32
Kerwin <zhouz88@uw.edu>
2019-09-17 00:19:43 -0700

    Update 1192. Critical Connections in a Network.java

--- 1192. Critical Connections in a Network.java
+++ 1192. Critical Connections in a Network.java
@@ -53,3 +53,48 @@ class Solution {
                 Arrays.asList( 2, 0))));
     }
 }
+
+
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
+        List<List<Integer>> result = new ArrayList<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (List<Integer> c : connections) {
+            map.putIfAbsent(c.get(0), new ArrayList<>());
+            map.get(c.get(0)).add(c.get(1));
+            map.putIfAbsent(c.get(1), new ArrayList<>());
+            map.get(c.get(1)).add(c.get(0));
+        }
+        dfsNext(0, map, new boolean[n], result, new int[n], new int[n], -1);
+        return result;
+    }
+
+    private int id = 0;
+
+    private boolean dfsNext(int start, Map<Integer, List<Integer>> map, boolean[] visited, List<List<Integer>> result
+    , int[] nodeIds, int[] minLevel, int parent) {
+        if (visited[start]) {
+            return false;
+        }
+        visited[start] = true;
+        nodeIds[start] = id;
+        minLevel[start] = id;
+        id++;
+        List<Integer> list = map.get(start);
+        for (int next : list) {
+            if (dfsNext(next, map, visited, result, nodeIds, minLevel, start)) {
+                minLevel[start] = Math.min(minLevel[start], minLevel[next]);
+                if (minLevel[next] > nodeIds[start]) {
+                    result.add(Arrays.asList(start, next));
+                }
+            } else {
+                if (nodeIds[next] != parent) {
+                    minLevel[start] = Math.min(nodeIds[next], minLevel[start]);
+                }
+            }
+        }
+        return true;
+    }
+}
