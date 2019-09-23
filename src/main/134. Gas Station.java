67a93767801d73d80dae6f6225135012a556b2d8
Kerwin <zhouz88@uw.edu>
2019-09-22 21:34:02 -0700

    Update 134. Gas Station.java

--- src/main/134. Gas Station.java
+++ src/main/134. Gas Station.java
@@ -1,21 +1,30 @@
-class Solution {//if total > 0 
+import java.util.Arrays;
+
+class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int total = 0;
-        int cur = 0;
+        boolean[] notStart = new boolean[gas.length];
+        int delta = 0;
         int start = 0;
-        for (int i = 0; i < gas.length; i++) {
-            total += gas[i] - cost[i];
-        }
-        
-        if (total < 0) return -1;
-        
-        for (int i = 0; i < gas.length; i++) {
-            cur += gas[i] - cost[i];
-            if (cur < 0) {
-                cur = 0;
-                start = i + 1;
+        int i = 0;
+        boolean flag = false;
+        while (true) {
+            flag = false;
+            delta += gas[i] - cost[i];
+            if (delta < 0) {
+                int j = start;
+                while (j != i) {
+                    if (notStart[j]) return -1;
+                    notStart[j] = true;
+                    j = (j + 1) % gas.length;
+                }
+                if (notStart[j]) return -1;
+                notStart[j] = true;
+                delta = 0;
+                start = (i + 1) % gas.length;
+                flag = true;
             }
+            i = (i + 1) % gas.length;
+            if (i == start && !flag) return i;
         }
-        return  start;
     }
 }
