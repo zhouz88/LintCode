cf049c593b1eac97519b15fabb54f4c1dab7dc9c
Kerwin <zhouz88@uw.edu>
2019-11-16 10:17:55 -0800

    Create WishPhoneInterview.java

---
+++ WishPhoneInterview.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    private int countZeros = 0;
+
+    int checkPairs(List<String> input) {
+        Map<Integer, Integer> start = new HashMap<>();
+        Map<Integer, Integer> end = new HashMap<>();
+        for (String k : input) {
+            check(k, start, end);
+        }
+        int res = 0;
+        for (int k : start.keySet()) {
+            if (end.containsKey(k)) {
+                res += start.get(k) * end.get(k);
+            }
+        }
+        return res + countZeros * (countZeros - 1);
+    }
+
+    private void check(String input, Map<Integer, Integer> start, Map<Integer, Integer> end) {
+        int cnt1 = 0;
+        boolean canStart = true;
+        boolean canEnd = true;
+        for (char ch : input.toCharArray()) {
+            if (ch == '(') cnt1++;
+            else cnt1--;
+            if (cnt1 < 0) {
+                canStart = false;
+            }
+        }
+        int cnt2 = 0;
+        String t = new StringBuilder(input).reverse().toString();
+        for (char ch : t.toCharArray()) {
+            if (ch == ')') cnt2++;
+            else cnt2--;
+            if (cnt2 < 0) {
+                canEnd = false;
+            }
+        }
+        if (canEnd && canStart) {
+            countZeros++;
+        } else if (canStart) {
+            start.put(cnt1, start.getOrDefault(cnt1, 0) + 1);
+        } else if (canEnd) {
+            end.put(cnt2, start.getOrDefault(cnt1, 0) + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().checkPairs(Arrays.asList("()", "()()", "(((()", ")))", "((()))", "((()(", ")(")));
+    }
+}
